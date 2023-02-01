bc1ed99f9069e312b971058efdb92546befb00b1
colinmfoster4723 <colin.foster4723@gmail.com>
2023-02-01 10:26:08 -0600

    Adjusted interface for env

--- ui/env.d.ts
+++ ui/env.d.ts
@@ -3,7 +3,7 @@ namespace NodeJS {
     DISCORD_CLIENT_ID: string
     DISCORD_CLIENT_SECRET: string
     GOOGLE_SHEETS_EMAIL: string
-    GOOGLE_SHEETS_SECRET: string
+    GOOGLE_SHEETS_SECRET: any
     GOOGLE_SPREADSHEET_ID: string
     TWITTER_CLIENT_ID: string
     TWITTER_CLIENT_SECRET: string
