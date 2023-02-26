35bd14cd69671f5d169f26cf26565587cfdfe7fb
colinmfoster4723 <colin.foster4723@gmail.com>
2023-02-26 15:38:12 -0600

    reset firebase keys

--- ui/lib/firebase.ts
+++ ui/lib/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: 'moondao-x-lifeship.appspot.com',
+  storageBucket: 'moondaoxlifeship.appspot.com',
   messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
 }
@@ -24,11 +24,3 @@ export async function hasUserSubmittedNFT(walletAddress: string) {
   const data = (await listAll(userRef)).items
   return data.length > 0 ? true : false
 }
-
-/*
-FIREBASE_API_KEY=AIzaSyBOxUCzDUi6pZH7vRQgPVs0YtMkQWuRy_0
-FIREBASE_AUTH_DOMAIN=moondao-x-lifeship.firebaseapp.com
-FIREBASE_PROJECT_ID=
-FIREBASE_STORAGE_BUCKET=moondao-x-lifeship.appspot.com
-FIREBASE_MSG_SENDER_ID=443395864085
-FIREBASE_APP_ID=1:443395864085:web:7e69ddaf956c8fddf2001c*/
