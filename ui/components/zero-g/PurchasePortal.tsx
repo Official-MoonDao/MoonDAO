f213536663dea70ccfff49a27b87129a5f91e458
colinmfoster4723 <colin.foster4723@gmail.com>
2023-03-31 14:22:20 -0500

    Adjusted purchase portal, added stage for promo code

--- ui/components/zero-g/PurchasePortal.tsx
+++ ui/components/zero-g/PurchasePortal.tsx
@@ -1,16 +1,48 @@
+import { useState } from 'react'
 import ReservationRaffleLayout from './ReservationRaffleLayout'
 
+function Button({ children, onClick }: any) {
+  return (
+    <button
+      className="border-style mt-4 tracking-wide btn text-n3blue normal-case font-medium font-GoodTimes w-full bg-transparent hover:bg-n3blue hover:text-white duration-[0.6s] ease-in-ease-out text-1xl"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
+}
+
 export default function PurchasePortal() {
+  const [state, setState] = useState(0)
+
   return (
     <ReservationRaffleLayout title="Purchase">
-      <button
-        onClick={() =>
-          window.open('https://www.gozerog.com/reservations/moondao-flight/')
-        }
-        className="border-style mt-4 tracking-wide btn text-n3blue normal-case font-medium font-GoodTimes w-full  bg-transparent hover:bg-n3blue hover:text-white duration-[0.6s] ease-in-ease-out text-1xl"
-      >
-        buy a ticket
-      </button>
+      {state === 0 && (
+        <>
+          <Button onClick={() => setState(1)}>buy a ticket</Button>
+        </>
+      )}
+      {state === 1 && (
+        <>
+          <div className="flex flex-col items-center">
+            <p className="text-center text-n3blue">
+              {`Use Promo Code "MOONDAO" at checkout to receive $2,500 off your flight!`}
+            </p>
+            <Button
+              onClick={() => {
+                window.open(
+                  'https://www.gozerog.com/reservations/moondao-spacefabw-flight/'
+                )
+                setTimeout(() => {
+                  setState(0)
+                }, 1000)
+              }}
+            >
+              continue
+            </Button>
+          </div>
+        </>
+      )}
     </ReservationRaffleLayout>
   )
 }
