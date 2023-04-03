9ff4cfc137fbb9c6832e7449ec338fb58ba69fb8
colinmfoster4723 <colin.foster4723@gmail.com>
2023-04-03 11:00:25 -0500

    adjusted zero-g purchase portal

--- ui/components/zero-g/PurchasePortal.tsx
+++ ui/components/zero-g/PurchasePortal.tsx
@@ -16,20 +16,20 @@ export default function PurchasePortal({ validLock }: any) {
   const [state, setState] = useState(0)
   const [error, setError] = useState(false)
 
+  useEffect(() => {
+    if (state === 2 && validLock) setState(1)
+  }, [validLock])
+
   return (
     <ReservationRaffleLayout title="Purchase">
       {state === 0 && (
         <>
-          {error && (
-            <p className="text-n3green ease-in-ease-out duration-300">{`Please connect a wallet that has vMooney`}</p>
-          )}
           <Button
             onClick={() => {
               //check if wallet has vMooney
               if (validLock) {
                 setState(1)
-                setError(false)
-              } else setError(true)
+              } else setState(2)
             }}
           >
             buy a ticket
@@ -57,6 +57,23 @@ export default function PurchasePortal({ validLock }: any) {
             </Button>
           </div>
         </>
+      )}
+      {state === 2 && (
+        <div>
+          <p className="text-n3blue ease-in-ease-out duration-300">{`Please connect a wallet that has vMooney to receive a discount code! Otherwise, press continue.`}</p>
+          <Button
+            onClick={() => {
+              window.open(
+                'https://www.gozerog.com/reservations/moondao-spacefabw-flight/'
+              )
+              setTimeout(() => {
+                setState(0)
+              }, 1000)
+            }}
+          >
+            continue
+          </Button>
+        </div>
       )}
     </ReservationRaffleLayout>
   )
