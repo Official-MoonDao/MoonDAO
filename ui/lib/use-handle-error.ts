1d5e688c4519a71bebd19845f4d98aee8ec4fe59
colinmfoster4723 <colin.foster4723@gmail.com>
2023-04-04 10:38:25 -0500

    Adjusted smart contract error handling, Issue #28

--- ui/lib/use-handle-error.ts
+++ ui/lib/use-handle-error.ts
@@ -4,12 +4,10 @@ import { useErrorContext } from '../components/layout/ErrorProvider'
 // For some contract interactions, a reverted call is not an error
 export function useHandleError(object: any, throwOnRevert = true) {
   const errorContext = useErrorContext()
+
   useEffect(() => {
     if (
-      !object.error?.message?.startsWith(
-        'missing revert data in call exception;'
-      ) &&
-      !object.reason?.includes('missing revert') &&
+      object?.error?.code !== 'CALL_EXCEPTION' &&
       throwOnRevert &&
       object.error
     ) {
