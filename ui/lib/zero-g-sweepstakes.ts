27e7d0d81c1434528c0656aee2bb0e2f8b19054b
colinmfoster4723 <colin.foster4723@gmail.com>
2023-05-06 15:36:09 -0500

    Adjusted gas limit for random selection

--- ui/lib/zero-g-sweepstakes.ts
+++ ui/lib/zero-g-sweepstakes.ts
@@ -24,7 +24,7 @@ export function useMintTicketZeroG() {
 export function useRandomSelection() {
   return useContractWrite(contractParams, 'chooseWinner', {
     overrides: {
-      gasLimit: 300000
+      gasLimit: 1000000
     },
   })
 }
