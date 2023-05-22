c1bfe97c686c26e1f1a76011e87b5c5f72f7b9f2
colinmfoster4723 <colin.foster4723@gmail.com>
2023-05-22 14:24:17 -0500

    Adjusted lifeship nft detail page

--- ui/pages/lifeship/detail.tsx
+++ ui/pages/lifeship/detail.tsx
@@ -7,12 +7,15 @@ export default function LifeshipNFTDetail() {
   return (
     <div className="animate-fadeIn">
       <WebsiteHead title="Lifeship NFT Details" />
-      <MainCard title="Lifeship NFT Details">
-        <p>{`MoonDAO is partnering with lifeship to send NFTs to space! Please upload a file and complete the checkout. You will be emailed instrucitons to claim your NFT.`}</p>
+      <MainCard title="">
+        <h1 className="font-GoodTimes text-3xl lg:text-4xl my-4">
+          Lifeship NFT Details
+        </h1>
+        <p className="">{`MoonDAO is partnering with lifeship to send NFTs to space! Please upload a file and complete the checkout. You will be emailed instrucitons to claim your NFT.`}</p>
         <div className="w-full flex justify-center">
           <button
             className="mt-4 flex items-center bg-[grey] text-lg rounded px-2 py-1 text-gray-100 hover:scale-[1.05] hover:text-white hover:bg-n3blue ease-in duration-150 w-1/4"
-            onClick={() => router.push('/zero-g')}
+            onClick={() => router.push('/lifeship')}
           >
             {'← Back'}
           </button>
