1f0ecf5373b3f21d966fe33ca23a0647641c558e
Elliot Briant <elliot@paysplit.io>
2023-10-26 12:47:55 -0300

    feature: saving latest changes

--- ui/components/onboarding/OnboardingStageManager.tsx
+++ ui/components/onboarding/OnboardingStageManager.tsx
@@ -39,92 +39,104 @@ export function OnboardingStageManager() {
     }
   }, [user])
 
+  const MultiStepStage = ({ steps }: any) => {
+    const handleNext = () => {
+      setStage(stage + 1);
+    }
+
+    const handlePrev = () => {
+      setStage(stage - 1);
+    }
+
+    return (
+      <div>
+        <div>
+          <button onClick={handlePrev} disabled={stage === 0}>Previous</button>
+          <br />
+          <button onClick={handleNext} disabled={stage === steps.length - 1}>Next</button>
+        </div>
+        <div>
+          <progress value={stage + 1} max={steps.length}></progress>
+        </div>
+        <div>
+          {steps[stage].component}
+        </div>
+      </div>
+    );
+  };
+
+  const StepZero = () => <StageContainer>
+    <h1 className="text-2xl">WELCOME TO MOONDAO</h1>
+    <p>{`Onboarding at MoonDAO takes less than five minutes even if it's your first time in Web3.`}</p>
+    <button
+      onClick={() => {
+        if (!user) {
+          login()
+        } else {
+          setStage(1)
+        }
+      }}
+      className="px-4 py-2 bg-white text-black"
+    >
+      Begin Onboarding
+    </button>
+  </StageContainer>;
+
+  const StepOne = () => <StageContainer>
+    <h1 className="text-2xl">Step 1 of 3: Select Contribution Level</h1>
+    <ContributionLevels
+      selectedLevel={selectedLevel}
+      setSelectedLevel={setSelectedLevel}
+    />
+    <PrivyWeb3Button
+      label="Purchase"
+      action={async () => {
+        //check balance of wallet, if not enough matic then fund from moonpay
+        // const maticBalance = await maticContract?.call('balanceOf', [
+        //   wallets[selectedWallet].address,
+        // ])
+        const provider = await wallets[selectedWallet].getEthersProvider()
+        const nativeBalance = await provider.getBalance(
+          wallets[selectedWallet].address
+        )
+
+        const formattedNativeBalance =
+          ethers.utils.formatEther(nativeBalance)
+
+        if (+formattedNativeBalance < selectedLevel) {
+          toast(
+            "You don't have enough Matic to purchase this level, use Moonpay to fund your wallet"
+          )
+          setTimeout(async () => {
+            await fund(selectedLevel - +formattedNativeBalance)
+          }, 3000)
+        }
+
+        //buy mooney on L2 using uniswap
+        const route = await generateRoute()
+        const tx = await executeRoute(route)
+        //approve mooney for lock
+        //lock mooney
+      }}
+      isDisabled={selectedLevel === 0}
+    />
+  </StageContainer>;
+
+  const StepTwo = () => <StageContainer>
+    <h1 className="text-2xl">Proof of Humanity</h1>
+    <p>{`To access governance and events at MoonDAO you must complete these steps.  No identifying data is stored by MoonDAO in this process.`}</p>
+    <ProofOfHumanity />
+  </StageContainer>;
+
+  const steps = [
+    { component: <StepZero /> },
+    { component: <StepOne /> },
+    { component: <StepTwo /> },
+  ];
+
   return (
     <div className="flex flex-col pt-8 w-full h-full">
-      {stage === 0 && (
-        <StageContainer>
-          <h1 className="text-2xl">WELCOME TO MOONDAO</h1>
-          <p>{`Onboarding at MoonDAO takes less than five minutes even if it's your first time in Web3.`}</p>
-          <button
-            onClick={() => {
-              if (!user) {
-                login()
-              } else {
-                setStage(1)
-              }
-            }}
-            className="px-4 py-2 bg-white text-black"
-          >
-            Begin Onboarding
-          </button>
-        </StageContainer>
-      )}
-      {stage === 1 && (
-        <StageContainer>
-          <h1 className="text-2xl">Step 1 of 3: Select Contribution Level</h1>
-          <ContributionLevels
-            selectedLevel={selectedLevel}
-            setSelectedLevel={setSelectedLevel}
-          />
-          <PrivyWeb3Button
-            label="Purchase"
-            action={async () => {
-              //check balance of wallet, if not enough matic then fund from moonpay
-              // const maticBalance = await maticContract?.call('balanceOf', [
-              //   wallets[selectedWallet].address,
-              // ])
-              const provider = await wallets[selectedWallet].getEthersProvider()
-              const nativeBalance = await provider.getBalance(
-                wallets[selectedWallet].address
-              )
-
-              const formattedNativeBalance =
-                ethers.utils.formatEther(nativeBalance)
-
-              if (+formattedNativeBalance < selectedLevel) {
-                toast(
-                  "You don't have enough Matic to purchase this level, use Moonpay to fund your wallet"
-                )
-                setTimeout(async () => {
-                  await fund(selectedLevel - +formattedNativeBalance)
-                }, 3000)
-              }
-
-              //buy mooney on L2 using uniswap
-              const route = await generateRoute()
-              const tx = await executeRoute(route)
-              //approve mooney for lock
-              //lock mooney
-            }}
-            isDisabled={selectedLevel === 0}
-          />
-        </StageContainer>
-      )}
-      {stage === 2 && (
-        <StageContainer>
-          <h1 className="text-2xl">Proof of Humanity</h1>
-          <p>{`To access governance and events at MoonDAO you must complete thtese steps.  No identifying data is stored by MoonDAO in this process.`}</p>
-          <ProofOfHumanity />
-        </StageContainer>
-      )}
-
-      {/* DEV BUTTONS -- REMOVE B4 PUSHING TO PROD */}
-      <div className="p-4 flex gap-2">
-        <button
-          className="p-2 bg-[blue]"
-          onClick={() => setStage(stage - 1)}
-          disabled={stage === 0}
-        >
-          prev stage
-        </button>
-        <button
-          className="p-2 bg-[blue]"
-          onClick={() => setStage(stage + 1)}
-          disabled={stage === 2}
-        >
-          next next
-        </button>
-      </div>
+      <MultiStepStage steps={steps} />
     </div>
   )
 }
