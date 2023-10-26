6d4cd131c049e1da2dcf34eb9fe06a9d9fbfc268
Elliot Briant <elliot@paysplit.io>
2023-10-26 13:47:32 -0300

    feature: saving test stepbar

--- ui/components/onboarding/OnboardingCongrats.tsx
+++ ui/components/onboarding/OnboardingCongrats.tsx
@@ -1,9 +1,9 @@
-import { SolarConfetti } from '../assets'
+// import { SolarConfetti } from '../assets'
 
 export function OnboardingCongrats() {
     return (
         <div className="flex flex-col font-RobotoMono w-full items-center gap-8">
-            <SolarConfetti />
+            {/* <SolarConfetti /> */}
             <div className='mt-9 lg:mt-7 flex flex-col items-center'>
                 <h1 className='font-GoodTimes text-2xl lg:text-5xl'>CONGRATULATIONS!</h1>
                 <p className='mt-[18px] text-[20px] lg:text-[32px] text-center'>You're in the MoonDAO community</p>
--- ui/components/onboarding/OnboardingStageManager.tsx
+++ ui/components/onboarding/OnboardingStageManager.tsx
@@ -56,6 +56,31 @@ export function OnboardingStageManager() {
           <br />
           <button onClick={handleNext} disabled={stage === steps.length - 1}>Next</button>
         </div>
+        <div className='w-100% h-100%'>
+          <ol className="flex items-center w-full">
+            <li className="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-blue-100 after:border-4 after:inline-block dark:after:border-blue-800">
+              <span className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
+                <svg className="w-3.5 h-3.5 text-blue-600 lg:w-4 lg:h-4 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                </svg>
+              </span>
+            </li>
+            <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
+              <span className="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+                <svg className="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                  <path d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
+                </svg>
+              </span>
+            </li>
+            <li className="flex items-center w-full">
+              <span className="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
+                <svg className="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                  <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z" />
+                </svg>
+              </span>
+            </li>
+          </ol>
+        </div>
         <div>
           <progress value={stage + 1} max={steps.length}></progress>
         </div>
