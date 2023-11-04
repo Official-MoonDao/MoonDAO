fb4ab1bf9864a9b47f863dfdfa424f6c2681d00c
colinmfoster4723 <colin.foster4723@gmail.com>
2023-11-04 15:16:42 -0500

    Added uniswap token config

---
+++ ui/lib/uniswap/UniswapTokens.tsx
@@ -0,0 +1,28 @@
+import { Ether, Token } from '@uniswap/sdk-core'
+import { MOONEY_ADDRESSES } from '../../const/config'
+
+export const ETH: any = Ether.onChain(1)
+
+export const MATIC = new Token(
+  137,
+  '0x0000000000000000000000000000000000001010',
+  18,
+  'MATIC',
+  'MATIC'
+)
+
+export const MOONEY = new Token(
+  1,
+  MOONEY_ADDRESSES['ethereum'],
+  18,
+  'MOONEY',
+  'MOONEY'
+)
+
+export const L2_MOONEY = new Token(
+  137,
+  MOONEY_ADDRESSES['polygon'],
+  18,
+  'MOONEY',
+  'MOONEY'
+)
