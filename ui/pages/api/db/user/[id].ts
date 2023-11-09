9351c1b184ca819c4486525d41ffe1708f42814a
Daniel <schwartzray8@gmail.com>
2023-11-08 20:52:03 -0800

    database and backend for users and Nfts

---
+++ ui/pages/api/db/user/[id].ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import User from '../../../../lib/mongodb/models/User'
+import dbConnect from '../../../../lib/mongodb/mongo'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    query: { id },
+    method,
+  } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET' /* Get a model by its ID */:
+      try {
+        const user = await User.findById(id)
+        if (!user) {
+          return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: user })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+
+    case 'PUT' /* Edit a model by its ID */:
+      try {
+        const user = await User.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        })
+        if (!user) {
+          return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: user })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+
+    case 'DELETE' /* Delete a model by its ID */:
+      try {
+        const deletedUser = await User.deleteOne({ _id: id })
+        if (!deletedUser) {
+          return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: {} })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
