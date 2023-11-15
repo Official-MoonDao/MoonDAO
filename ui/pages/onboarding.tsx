1d79b80db028a4b67cc4380a991cd5027ea160be
colinmfoster4723 <colin.foster4723@gmail.com>
2023-11-14 18:39:27 -0600

    Adjusted ssg

--- ui/pages/onboarding.tsx
+++ ui/pages/onboarding.tsx
@@ -21,7 +21,7 @@ export default function Onboarding({ pregenSwapRoutes }: any) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   // USD to Native uniswap quotes
   const explorerNativeRoute = await pregenSwapRoute(
     CONTRIBUTION_LEVEL_PRICES[0] * 12,
