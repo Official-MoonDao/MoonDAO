774b8ec969e27ff9908fc91442ee0b033e304371
colinmfoster4723 <colin.foster4723@gmail.com>
2023-11-30 14:00:54 -0600

    Adjusted contribution levels for testing

--- ui/components/onboarding/ContributionLevels.tsx
+++ ui/components/onboarding/ContributionLevels.tsx
@@ -160,7 +160,7 @@ export function ContributionLevels({
         icon="/explorer.svg"
         title="Explorer"
         intro="Perfect for those that want to dip their feet into the MoonDAO community."
-        mooneyValue={40000}
+        mooneyValue={100}
         points={[
           'Can purchase two Ticket to Space Sweepstakes Entries',
           'Community Discord Access',
@@ -172,7 +172,7 @@ export function ContributionLevels({
         icon="/citizen.svg"
         title="Citizen"
         intro="Take an active seat in the construction of the largest network-state focused on becoming multi-planetary."
-        mooneyValue={500000}
+        mooneyValue={2500}
         points={[
           'Can purhcase up to 12 Ticket To Space Entries',
           'Exclusive Discord Access',
