94d2720191c3647829f37bff88f6d0f66d3212b4
colinmfoster4723 <colin.foster4723@gmail.com>
2023-12-05 14:45:04 -0600

    Adjusted env var

--- ui/lib/thirdweb/thirdweb.ts
+++ ui/lib/thirdweb/thirdweb.ts
@@ -4,6 +4,6 @@ import { ThirdwebSDK } from '@thirdweb-dev/sdk'
 export function initSDK(chain: Chain) {
   return new ThirdwebSDK(chain, {
     clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
-    secretKey: process.env.NEXT_PUBLIC_THIRDWEB_SECRET_KEY,
+    secretKey: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_SECRET,
   })
 }
