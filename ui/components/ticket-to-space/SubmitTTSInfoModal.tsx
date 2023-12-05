25f5854e2fe78f62f592d370596435b70889884a
colinmfoster4723 <colin.foster4723@gmail.com>
2023-12-04 18:22:17 -0600

    Updated db call

--- ui/components/ticket-to-space/SubmitTTSInfoModal.tsx
+++ ui/components/ticket-to-space/SubmitTTSInfoModal.tsx
@@ -41,7 +41,7 @@ export function SubmitTTSInfoModal({
         headers: {
           'Content-Type': 'application/json',
           'moondao-api-key': process.env.NEXT_PUBLIC_MONGO_MOONDAO_API_KEY,
-        } as any,
+        },
         body: JSON.stringify({
           tokenId,
           email,
@@ -186,7 +186,15 @@ export function SubmitTTSInfoModal({
                     return setIsLoading(false)
                   }
                   //find owned tokenIds that aren't in the database yet
-                  const verifiedNftsRes = await fetch('/api/db/nft')
+                  const verifiedNftsRes = await fetch('/api/db/nft', {
+                    method: 'GET',
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'moondao-api-key':
+                        process.env.NEXT_PUBLIC_MONGO_MOONDAO_API_KEY,
+                    },
+                  })
+
                   const { data: verifiedNfts } = await verifiedNftsRes.json()
 
                   const nftsNotInDatabase = ownedNfts.filter(
