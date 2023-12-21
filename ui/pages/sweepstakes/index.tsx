5eb7b25dc6810d7dac0498c60e221e1f2567ec11
colinmfoster4723 <colin.foster4723@gmail.com>
2023-12-21 10:36:17 -0600

    Adjusted text

--- ui/pages/sweepstakes/index.tsx
+++ ui/pages/sweepstakes/index.tsx
@@ -7,6 +7,7 @@ import {
   useOwnedNFTs,
 } from '@thirdweb-dev/react'
 import { BigNumber, ethers } from 'ethers'
+import Link from 'next/link'
 import { useContext, useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import ChainContext from '../../lib/thirdweb/chain-context'
@@ -112,40 +113,28 @@ export default function Sweepstakes() {
           Take the leap, for the chance to win a trip to space!
         </h3> */}
         <p className="mt-5 bg-[#CBE4F7] text-[#1F212B] dark:bg-[#D7594F36] dark:text-white  px-2 py-2 xl:py-3 xl:px-4 2xl:max-w-[750px] xl:text-left text-sm xl:text-base">
-          {`
-        Take the leap, for the chance to win an opportunity to fly to space! We're selecting one lucky member of the MoonDAO community to win. If you're not a member of our community`}
-          <a className="text-moon-gold" href="/join" target="_blank">
-            {' join MoonDAO now '}
-          </a>
-          {`to enter.`}
-        </p>
-
-        <p className="mt-5 bg-[#CBE4F7] text-[#1F212B] dark:bg-[#D7594F36] dark:text-white  px-2 py-2 xl:py-3 xl:px-4 2xl:max-w-[750px] text-center xl:text-left text-sm xl:text-base">
-          {`The Ticket to Space Sweepstakes is on Polygon. Check out this`}
-          <a
+          {`One person will be randomly selected to win an opportunity aboard a future Blue Origin rocket to space! Each NFT costs 20,000 $MOONEY (`}
+          <Link
+            className="text-moon-gold"
+            href="https://www.youtube.com/watch?v=VxU8dOrL0fE"
+            target="_blank"
+          >
+            {'Watch Tutorial'}
+          </Link>
+          {`), aquire some `}
+          <Link className="text-moon-gold" href="/join" target="_blank">
+            {' by joining MoonDAO'}
+          </Link>
+          {`. If you already have $MOONEY, `}
+          <Link
             className="text-moon-gold"
             href="https://youtu.be/oQtHjbcbAio?feature=shared"
+            target="_blank"
           >
-            {' '}
-            guide
-          </a>{' '}
-          and bridge your $MOONEY
-          <a
-            className="text-moon-gold"
-            href="https://wallet.polygon.technology/polygon/bridge/deposit"
-          >
-            {' '}
-            here
-          </a>
-          .
+            {' bridge your tokens '}
+          </Link>
+          {`to the Polygon PoS Network. You can mint up to 50 entries.`}
         </p>
-
-        <div className="">
-          <p className="mt-5 text-sm lg:mt-6 opacity-70 max-w-2xl lg:max-w-3xl font-RobotoMono text-center lg:text-left lg:text-base xl:text-lg">
-            {`
-            We will randomly select an individual to win an opportunity to fly to space on a future Blue Origin rocket launch! Every individual may mint up to 50 entries.`}
-          </p>
-        </div>
 
         {/*Collection title, image and description*/}
         <div className="mt-6 inner-container-background relative w-full">
