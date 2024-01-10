9b007677b8bfdceb49b32dcfec1ed157136af03c
danielschwartz4 <52050264+danielschwartz4@users.noreply.github.com>
2024-01-10 09:52:33 -0800

    convert kit api (#56)
    * convert kit api
    
    * added tagging
    
    * finished tagging
    
    * added first name field

--- ui/components/ticket-to-space/SubmitTTSInfoModal.tsx
+++ ui/components/ticket-to-space/SubmitTTSInfoModal.tsx
@@ -290,6 +290,18 @@ export function SubmitTTSInfoModal({
                   return setEnabled(false)
                 }
 
+                await fetch('/api/convertkit', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'moondao-api-key': signature,
+                  },
+                  body: JSON.stringify({
+                    userEmail: email,
+                    name: fullName,
+                  }),
+                })
+
                 toast.success('Your NFT(s) have been verified and registered!')
 
                 setEnabled(false)
