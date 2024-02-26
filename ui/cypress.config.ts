8122228344e180751ef955c6ecb836cff1e72df6
colinmfoster4723 <colin.foster4723@gmail.com>
2024-02-26 11:40:29 -0600

    Added types and tests for gradient link

--- ui/cypress.config.ts
+++ ui/cypress.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     //   'cypress/integration/layout/main-card.cy.tsx',
     //   'cypress/integration/lock/lock-data.cy.tsx',
     //   'cypress/integration/lock/balance.cy.tsx',
+    //   'cypress/integration/lock/time-range.cy.tsx',
     //   'cypress/integration/privy/privy-connect-wallet.cy.tsx',
     //   'cypress/integration/privy/privy-web3-button.cy.tsx',
     //   'cypress/integration/shopify/product.cy.tsx',
