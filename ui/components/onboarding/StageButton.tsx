e521cf95fc8fcffbe0530338485c3be5ac1d3b1d
colinmfoster4723 <colin.foster4723@gmail.com>
2024-03-10 14:01:04 -0500

    Added loading state

--- ui/components/onboarding/StageButton.tsx
+++ ui/components/onboarding/StageButton.tsx
@@ -1,10 +1,18 @@
-export function StageButton({ onClick, children, isDisabled, isLoading }: any) {
+import { useState } from 'react'
+
+export function StageButton({ onClick, children, isDisabled }: any) {
+  const [isLoading, setIsLoading] = useState(false)
+
   return (
     <button
       className={
         'mt-8 px-4 py-2 w-[300px] border-2 border-moon-orange text-moon-orange'
       }
-      onClick={onClick}
+      onClick={async () => {
+        setIsLoading(true)
+        await onClick()
+        setIsLoading(false)
+      }}
       disabled={isDisabled || isLoading}
     >
       {isLoading ? 'loading...' : children}
