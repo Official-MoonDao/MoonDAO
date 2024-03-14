6a621ad98f49a3dfa6aaea2e07c1eb9366623810
colinmfoster4723 <colin.foster4723@gmail.com>
2024-03-14 11:51:50 -0500

    Updated documentation links

--- ui/components/layout/Sidebar/Navigation.ts
+++ ui/components/layout/Sidebar/Navigation.ts
@@ -28,7 +28,7 @@ export const navigation = [
       { name: 'Lock $MOONEY', href: '/lock' },
       {
         name: 'Constitution',
-        href: 'https://publish.obsidian.md/moondao/MoonDAO/docs/Constitution',
+        href: 'https://docs.moondao.com/Constitution',
       },
       {
         name: 'Submit Proposal',
