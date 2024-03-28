76e7be8539fa1a6262ea79f70048c0f7cad452e1
colinmfoster4723 <colin.foster4723@gmail.com>
2024-03-27 19:20:48 -0500

    Updated test

--- ui/cypress/integration/layout/steps.cy.tsx
+++ ui/cypress/integration/layout/steps.cy.tsx
@@ -8,6 +8,8 @@ describe('<Steps />', () => {
         id="test-steps"
         steps={['Step 1', 'Step 2', 'Step 3']}
         currStep={0}
+        lastStep={2}
+        setStep={() => {}}
       />
     )
     cy.get('#test-steps').should('exist')
