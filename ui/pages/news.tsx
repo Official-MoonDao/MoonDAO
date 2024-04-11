606c93db651b2f06476dd4b308f2ac75134388c3
colinmfoster4723 <colin.foster4723@gmail.com>
2024-04-11 14:45:30 -0500

    Added metadata

--- ui/pages/news.tsx
+++ ui/pages/news.tsx
@@ -1,6 +1,12 @@
+import Head from '../components/layout/Head'
+
 export default function News() {
   return (
     <div className="flex flex-col justify-center items-center animate-fadeIn lg:px-3 lg:pb-14 lg:mt-1 md:max-w-[1080px]">
+      <Head
+        title="News"
+        description="Stay informed with the latest news and updates from MoonDAO"
+      />
       <iframe
         className="absolute top-0 left-0 h-[110vh] overflow-auto w-full"
         src="https://moondao.ck.page/profile/posts"
