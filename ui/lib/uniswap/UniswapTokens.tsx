9a7ee990814ef3f79a2eac8f7adb45dcc80199dd
colinmfoster4723 <colin.foster4723@gmail.com>
2024-04-25 16:46:26 -0500

    Refactor

--- ui/lib/uniswap/UniswapTokens.tsx
+++ ui/lib/uniswap/UniswapTokens.tsx
@@ -1,32 +1,28 @@
-import { TokenERC20History } from '@thirdweb-dev/sdk'
-import { Ether, Token } from '@uniswap/sdk-core'
+import { Token } from '@uniswap/sdk-core'
 import { nativeOnChain } from '@uniswap/smart-order-router'
-import { useContext, useMemo } from 'react'
+import { useContext, useEffect, useMemo, useState } from 'react'
 import { DAI_ADDRESSES, MOONEY_ADDRESSES } from '../../const/config'
 import ChainContext from '../thirdweb/chain-context'
 
-export function useUniswapTokens() {
-  const { selectedChain } = useContext(ChainContext)
+export function useUniswapTokens(selectedChain: any) {
+  const NATIVE_TOKEN = useMemo(() => {
+    return nativeOnChain(selectedChain.chainId)
+  }, [selectedChain.chainId])
 
-  const NATIVE_TOKEN: any = nativeOnChain(selectedChain.chainId)
   const MOONEY = useMemo(() => {
     return new Token(
       selectedChain.chainId,
       MOONEY_ADDRESSES[selectedChain.slug],
-      18,
-      'MOONEY',
-      'MOONEY'
+      18
     )
-  }, [selectedChain])
+  }, [selectedChain.chainId])
   const DAI = useMemo(() => {
     return new Token(
       selectedChain.chainId,
       DAI_ADDRESSES[selectedChain.slug],
-      18,
-      'DAI',
-      'DAI Stablecoin'
+      18
     )
-  }, [selectedChain])
+  }, [selectedChain.chainId])
 
   return { MOONEY, NATIVE_TOKEN, DAI }
 }
