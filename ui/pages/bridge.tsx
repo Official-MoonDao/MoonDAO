bd6c24558c80a9c0347c3e54528efcf28ffffdc4
colinmfoster4723 <colin.foster4723@gmail.com>
2024-04-29 16:15:06 -0500

    Adjusted default chain

--- ui/pages/bridge.tsx
+++ ui/pages/bridge.tsx
@@ -1,18 +1,22 @@
+import { Ethereum, Sepolia } from '@thirdweb-dev/chains'
 import { useContract } from '@thirdweb-dev/react'
 import { MOONEY_ADDRESSES } from 'const/config'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import ChainContext from '@/lib/thirdweb/chain-context'
+import { useShallowQueryRoute } from '@/lib/utils/hooks'
 import ArbitrumBridge from '@/components/bridge/ArbitrumBridge'
 import NetworkSelector from '@/components/thirdweb/NetworkSelector'
 
 export default function Bridge() {
   const { selectedChain, setSelectedChain } = useContext(ChainContext)
 
+  useEffect(() => {
+    setSelectedChain(Ethereum)
+  }, [])
+
   return (
     <div className="animate-fadeIn">
-      <NetworkSelector />
-
-      <h1>Bridge</h1>
+      <h1>Arbitrum Bridge</h1>
       <ArbitrumBridge setSelectedChain={setSelectedChain} />
     </div>
   )
