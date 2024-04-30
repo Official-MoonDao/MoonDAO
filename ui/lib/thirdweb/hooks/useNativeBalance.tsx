26f81001f89c7cb196007f6486fd2abc848183c5
colinmfoster4723 <colin.foster4723@gmail.com>
2024-04-30 16:09:04 -0500

    Adjusted provider

--- ui/lib/thirdweb/hooks/useNativeBalance.tsx
+++ ui/lib/thirdweb/hooks/useNativeBalance.tsx
@@ -1,22 +1,26 @@
 import { useWallets } from '@privy-io/react-auth'
+import { useSDK } from '@thirdweb-dev/react'
 import { useContext, useEffect, useState } from 'react'
 import PrivyWalletContext from '../../privy/privy-wallet-context'
+import ChainContext from '../chain-context'
 
 export function useNativeBalance() {
   const { selectedWallet } = useContext(PrivyWalletContext)
+  const { selectedChain } = useContext(ChainContext)
   const { wallets } = useWallets()
+  const sdk = useSDK()
 
   const [nativeBalance, setNativeBalance] = useState<any>()
 
   async function getNativeBalance() {
-    const provider = await wallets[selectedWallet].getEthersProvider()
+    const provider = await sdk.getProvider()
     const balance = await provider.getBalance(wallets[selectedWallet].address)
     setNativeBalance((+balance / 10 ** 18).toFixed(5))
   }
 
   useEffect(() => {
     if (wallets[selectedWallet]) getNativeBalance()
-  }, [wallets, selectedWallet])
+  }, [wallets, selectedWallet, selectedChain])
 
   return nativeBalance
 }
