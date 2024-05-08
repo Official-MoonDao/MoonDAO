7dfa6cf265a59b38473a8b39548069426960284f
colinmfoster4723 <colin.foster4723@gmail.com>
2024-05-08 11:51:47 -0500

    Adjusted styling

--- ui/components/layout/PageCards.tsx
+++ ui/components/layout/PageCards.tsx
@@ -18,7 +18,7 @@ export function PageCards({
   const router = useRouter()
 
   return (
-    <div className="mt-3 px-5 lg:px-8 xl:px-9 py-12 lg:py-14 page-border-and-color w-[336px] sm:w-[400px] lg:mt-10 lg:w-full lg:max-w-[1080px] font-[Lato]">
+    <div className="mt-3 px-5 lg:px-8 xl:px-9 py-12 lg:py-14 page-border-and-color w-[336px] sm:w-[400px] lg:mt-10 lg:w-[600px] lg:max-w-[1080px] font-[Lato] flex flex-col">
       {header && (
         <p className="text-[#071732] dark:text-white font-RobotoMono font-semibold text-sm lg:text-base text-center lg:text-left">
           {header}
