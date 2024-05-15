50c28d8f903ae6e104c369fa750fe5d79a11895b
colinmfoster4723 <colin.foster4723@gmail.com>
2024-05-15 11:26:40 -0500

    Adjusted next-sitemap config

--- ui/next-sitemap.config.js
+++ ui/next-sitemap.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   siteUrl: 'app.moondao.com', // Replace with your site URL
   generateRobotsTxt: true, // Set to generate robots.txt
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: '',
+      },
+    ],
+  },
 }
