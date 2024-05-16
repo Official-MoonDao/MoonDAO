073dbb3966a0afb37e2f3e902b5dcaecc36e80f7
jigglyjams <jigglyjams@protonmail.com>
2024-05-15 20:29:38 -0400

    feat(newProposal): init sign proposal, update nance-sdk

--- ui/pages/newProposal.tsx
+++ ui/pages/newProposal.tsx
@@ -5,8 +5,14 @@ import "@nance/nance-editor/lib/css/editor.css";
 import "@nance/nance-editor/lib/css/dark.css";
 import ProposalTitleInput from "../components/nance/ProposalTitleInput";
 import { TEMPLATE } from "../lib/nance";
+import { EIP712Domain, signTypedDataInternal, useSigner } from "@thirdweb-dev/react";
+import { SnapshotTypes, domain, formatSnapshotProposalMessage } from "@nance/nance-sdk";
+import { Proposal } from "@nance/nance-sdk";
 
 export default function NewProposal() {
+  const signer = useSigner()
+  
+  // Nance Editior
   let getMarkdown: GetMarkdown;
   let setMarkdown: SetMarkdown;
 
@@ -40,6 +46,42 @@ export default function NewProposal() {
           fileUploadIPFS={fileUploadIPFS}
           darkMode={true}
         />
+        <div className="mt-3 flex justify-end">
+          {/* Submit buttons */}
+          <div className="flex justify-end space-x-5">
+            {/*  DRAFT */}
+            <button
+              type="button"
+              className={`text-sm px-5 py-3 border border-dashed border-moon-orange font-RobotoMono rounded-sm hover:rounded-tl-[22px] hover:rounded-br-[22px] duration-300`}
+              onClick={async () => {
+                if (!signer) return
+                const address = await signer.getAddress()
+                const message = formatSnapshotProposalMessage(
+                  address,
+                  {
+                    title: "Proposal Title",
+                    body: getMarkdown(),
+                  } as Proposal,
+                  "tomoondao.eth",
+                  new Date(),
+                  new Date()
+                )
+                console.log(message)
+                const signature = await signTypedDataInternal(signer, domain as EIP712Domain, SnapshotTypes.proposalTypes, message)
+                console.log(signature)
+              }}
+            >
+              Save Draft
+            </button>
+            {/* SUBMIT */}
+            <button
+              type="button"
+              className={`px-5 py-3 bg-moon-orange border border-transparent font-RobotoMono rounded-sm hover:rounded-tl-[22px] hover:rounded-br-[22px] duration-300`}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
