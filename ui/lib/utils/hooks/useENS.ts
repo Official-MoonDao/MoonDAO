28ac1b405c7a57e1707f7d868925a21d57b1dbe1
twodam <twodam@protonmail.com>
2024-05-23 10:30:57 +0800

    feat(useENS): resolve ENS through ensideas

--- ui/lib/utils/hooks/useENS.ts
+++ ui/lib/utils/hooks/useENS.ts
@@ -1,20 +1,23 @@
-import { Ethereum } from '@thirdweb-dev/chains'
-import { useEffect, useState } from 'react'
-import { initSDK } from '../../thirdweb/thirdweb'
+import useSWR from 'swr'
+import { isAddress } from 'ethers/lib/utils'
+
+interface ENSIdeaResponse {
+  address: string
+  name: string
+  displayName: string
+  avatar: string
+}
 
 export function useENS(address: string = '') {
-  const [ens, setENS] = useState<string | null>()
+  const { data, error } = useSWR(
+    isAddress(address)
+      ? `https://api.ensideas.com/ens/resolve/${address}`
+      : null,
+    (url) =>
+      fetch(url)
+        .then((r) => r.json())
+        .then((j) => j as ENSIdeaResponse)
+  )
 
-  async function getENS() {
-    const sdk = initSDK(Ethereum)
-    const provider = sdk.getProvider()
-    const ensLookup = await provider.lookupAddress(address)
-    setENS(ensLookup)
-  }
-
-  useEffect(() => {
-    if (address) getENS()
-  }, [address])
-
-  return ens
+  return data?.name
 }
