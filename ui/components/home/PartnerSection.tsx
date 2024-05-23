e553002f7983effbcd5777c641985d6cf7c0013b
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-05-23 11:39:51 -0400

    Updates

--- ui/components/home/PartnerSection.tsx
+++ ui/components/home/PartnerSection.tsx
@@ -6,7 +6,7 @@ export default function PartnerSection() {
       <div className=" w-full pt-20 pb-5 flex items-center flex-col max-w-[1200px]">
         <h2 className="header font-GoodTimes text-dark-cool">Our Partners</h2>
         <div className=" w-full max-w-[1200px] m-5 mb-0">  
-          <div className="p-5 pt-0 flex flex-rowflex flex-row flex-wrap justify-center">
+          <div className="p-5 pt-0 flex flex-row flex flex-row flex-wrap justify-center">
           <Partner alt="Blue Origin Logo" logo="assets\logo-blue-origin.svg" />
           <Partner alt="Lifeship Logo" logo="assets\logo-lifeship.svg" />
           <Partner alt="Space For A Better World Logo" logo="assets\logo-space-for-a-better-world.png" />
