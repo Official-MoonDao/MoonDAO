fe6845db8c50dd3019731b2d7cb989c101676a23
twodam <twodam@protonmail.com>
2024-05-23 20:33:59 +0800

    feat(ProposalPage): add skeleton view

--- ui/pages/proposal/[proposal].tsx
+++ ui/pages/proposal/[proposal].tsx
@@ -4,9 +4,48 @@ import { NANCE_API_URL, NANCE_SPACE_NAME } from '../../lib/nance/constants'
 import { useVotesOfProposal } from '../../lib/snapshot'
 import DropDownMenu from '../../components/nance/DropdownMenu'
 import MarkdownWithTOC from '../../components/nance/MarkdownWithTOC'
-import ProposalInfo from '../../components/nance/ProposalInfo'
+import ProposalInfo, {
+  ProposalInfoSkeleton,
+} from '../../components/nance/ProposalInfo'
 import ProposalSummary from '../../components/nance/ProposalSummary'
 import ProposalVotes from '../../components/nance/ProposalVotes'
+
+function ProposalSkeleton() {
+  return (
+    <div className="absolute top-0 left-0 lg:left-[20px] h-[100vh] overflow-auto w-full lg:px-10 page-border-and-color font-[Lato]">
+      <header className="relative isolate">
+        <div
+          className="absolute inset-0 -z-10 overflow-hidden"
+          aria-hidden="true"
+        >
+          <div className="absolute left-16 top-full -mt-16 transform-gpu opacity-50 blur-3xl xl:left-1/2 xl:-ml-80">
+            <div
+              className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#FF80B5] to-[#9089FC]"
+              style={{
+                clipPath:
+                  'polygon(100% 38.5%, 82.6% 100%, 60.2% 37.7%, 52.4% 32.1%, 47.5% 41.8%, 45.2% 65.6%, 27.5% 23.4%, 0.1% 35.3%, 17.9% 0%, 27.7% 23.4%, 76.2% 2.5%, 74.2% 56%, 100% 38.5%)',
+              }}
+            />
+          </div>
+          <div className="absolute inset-x-0 bottom-0 h-px bg-gray-900/5" />
+        </div>
+
+        <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8 lg:py-10">
+          <div className="flex flex-row justify-between">
+            <ProposalInfoSkeleton />
+          </div>
+        </div>
+      </header>
+
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 sm:py-16 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {/* Proposal */}
+          <div className="h-[600px] animate-pulse inner-container-background shadow-sm ring-1 ring-gray-900/5 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-16"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function Proposal() {
   const router = useRouter()
@@ -26,7 +65,7 @@ function Proposal() {
   )
 
   if (isLoading) {
-    return <p>Loading</p>
+    return <ProposalSkeleton />
   } else if (!proposalPacket) {
     return <p>Proposal not found</p>
   }
