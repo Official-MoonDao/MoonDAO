71ebad711eb17da816f4e71c38079d3827afecb0
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-05-24 14:20:59 -0400

    Updates

--- ui/components/layout/Template.tsx
+++ ui/components/layout/Template.tsx
@@ -1,8 +1,8 @@
-import Footer from '../pages/Footer'; 
+import Footer from '../../pages/Footer'; 
 
 export default function Template() {
   return ( 
-    <section className="PAGE-SECTION overflow-visible z-10 animate-fadeIn w-full h-[100vh] overflow-auto no-bar">
+    <section id="page-section" className="overflow-visible z-10 animate-fadeIn w-full h-[100vh] overflow-auto no-bar">
       <div className="CONTAINER w-full h-[100vh]">
           <div className="BACKGROUND-ELEMENTS ">
             <div className="BASE-BG gradient-1 w-full h-[100vh] absolute top-0 left-0"></div>     
---
+++ ui/components/layout/TitleSection.tsx
@@ -0,0 +1,11 @@
+export default function TitleSection() {
+    return ( 
+      <section className="TITLE-CONTAINER">
+        <h1 className="HEADER-ELEMENT header">Header</h1>
+        <h2 className="SUB-HEADER-ELEMENT sub-header">Sub-header</h2>
+        <p>Description</p>
+
+      </section>
+    )
+  }
+  
