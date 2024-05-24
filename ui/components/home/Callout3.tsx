d6f9edb7919930bd43c4c6b5e6941887edfd17b2
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-05-24 13:34:57 -0400

    Updates

--- ui/components/home/Callout3.tsx
+++ ui/components/home/Callout3.tsx
@@ -9,43 +9,43 @@ export default function Callout3() {
                                 <div className="p-0 pb-5 md:p-2.5 md:pl-0 md:pt-0">
                                         <Card
                                                 icon="/assets/icon-astronaut.svg" 
-                                                header="Sending Two Members To Space" 
-                                                paragraph="Sent the first crowdraised astronaut to space, through a democratically governed onchain election"
+                                                header="Sending Members To Space" 
+                                                paragraph="Sent the first crowdraised astronaut to space, through a democratically governed onchain vote. Randomly chose a second regular, everyday person who will be flying in the near future."
                                         />
                                 </div>
                                 <div className="p-0 pb-5 md:p-2.5 md:pr-0 md:pt-0 lg:pr-2.5">
                                         <Card 
                                                 icon="/assets/icon-ethereum.svg" 
                                                 header="Funding Open Space R&D" 
-                                                paragraph="Allocated $100,000+ (50+ ETH) to space R&D projects through our community governance, like open source time standards for PNT on the Moon (shortlisted by DARPA for a grant) "
+                                                paragraph="Allocated $100,000+ (50+ ETH) to space R&D projects through community governance, like open source time standards for PNT on the Moon (shortlisted by DARPA for a grant)."
                                         />
                                 </div>
                                 <div className="p-0 pb-5 md:p-2.5 md:pl-0 lg:pl-2.5 lg:pr-0 lg:pt-0">
                                         <Card 
                                                 icon="/assets/icon-plane.svg" 
                                                 header="Astronaut Training Program" 
-                                                paragraph="Sent the first crowdraised astronaut to space, through a democratically governed onchain election."
+                                                paragraph="Training future space travelers with innovative programs, like chartering an entire zero gravity flight alongside three NASA astronauts, Charlie Duke, Nicole Stott, and Doug Hurley."
                                         /> 
                                 </div>
                                 <div className="p-0 pb-5 md:p-2.5 md:pr-0 lg:pl-0 lg:pr-2.5 lg:pb-0">
                                         <Card 
                                                 icon="/assets/icon-dao.svg" 
-                                                header="Borderless Access To Space" 
-                                                paragraph="Developed a first-of-its-kind open-source DAO operating system to improve community governance, onboarding, and coordination"
+                                                header="Borderless Space Participation" 
+                                                paragraph="Developed a first-of-its-kind open-source DAO operating system to improve community governance, onboarding, and coordination, making it possible for global participation in space."
                                         />                   
                                 </div>
                                 <div className="p-0 pb-5 md:p-2.5 md:pl-0 md:pb-0 lg:pl-2.5">                  
                                         <Card 
                                                 icon="/assets/icon-governance.svg" 
                                                 header="First Moon Mission Planned" 
-                                                paragraph="Established a constitution for self-governance, which will be sent to the surface of the Moon in 2024 with LifeShip. See the video"
+                                                paragraph="Established a constitution for self-governance, which will be sent to the surface of the Moon in late-2024 as part of a LifeShip capsule alongside the DNA of MoonDAO members."
                                         />      
                                 </div>
                                 <div className="p-0 md:p-2.5 md:pr-0 md:pb-0">
                                         <Card 
-                                                icon="/assets/icon-ticket.svg" 
-                                                header="Hosted Sweepstakes" 
-                                                paragraph="Launched the Ticket To Space Sweepstakes to select an everyday person to go to space"
+                                                icon="/assets/icon-governance.svg" 
+                                                header="Transparent Governance System" 
+                                                paragraph="We believe in being open, including open source, transparency, and decentralization. As a DAO, we utilize blockchain technologies offering full transparency and accountability."
                                         />                 
                                 </div>
                         </div>
