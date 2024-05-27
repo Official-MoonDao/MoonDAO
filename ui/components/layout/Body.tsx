03b0825023c29d9bec36a94be37ae4ba0d2fad82
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-05-27 19:09:52 -0400

    Updates

--- ui/components/layout/Body.tsx
+++ ui/components/layout/Body.tsx
@@ -8,11 +8,11 @@ interface BodyProps {
 
 function Body({ children, fullWidth }: BodyProps) {
   return (
-    <div id="body-element-section" className={`overflow-auto no-bar h-full min-h-[100vh] flex flex-col fixed top-0 ${fullWidth ? 'ml-5 z-50 w-full gradient-9 left-0' : 'mt-[50px] md:mt-0 right-0 w-[calc(100vw-20px)] md:w-[calc(100vw-260px)] lg:w-[calc(100vw-290px)]'}`}>
+    <div id="body-element-section" className={`overflow-auto no-bar h-full min-h-[100vh] flex flex-col fixed top-0 ${fullWidth ? 'pl-5 z-50 w-full gradient-9 left-0' : 'mt-[50px] md:mt-0 right-0 w-[calc(100vw-20px)] md:w-[calc(100vw-260px)] lg:w-[calc(100vw-290px)]'}`}>
       <div id="content-container-section" className="flex-grow">
         {children}
       </div>  
-      <div id="footer-container" className="min-h-[150px] md:min-h-0">
+      <div id="footer-container" className="">
         <Footer />
       </div>
     </div>
