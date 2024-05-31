8f6a4a72e45e3945c37ac331d1cb1d7387349f79
colinmfoster4723 <colin.foster4723@gmail.com>
2024-05-31 11:00:18 -0500

    Added block lists

--- ui/const/whitelist.ts
+++ ui/const/whitelist.ts
@@ -1,27 +1,3 @@
-export const approvedEntities = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-]
+export const blockedEntities: any = []
 
-export const approvedCitizens = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-]
+export const blockedCitizens: any = []
