5f11d13bdf614560badbdb0a819435b22941a91a
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-06-04 20:11:43 -0400

    Welcome page design updates
    Updated elements for better handling on large, wide, zoomed-out monitors

--- ui/components/home/Callout2.tsx
+++ ui/components/home/Callout2.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link'
 import StandardButton from '../layout/StandardButton'
 
 export default function Callout2() {
   return ( 
     <section>
-        <div id="callout2-container" className=" gradient-4 z-10 md:rounded-tl-[5vmax] relative flex flex-col items-end lg:items-start justify-end pt-0 lg:pt-5 p-5 pb-[10vmax] md:pr-10 md:pl-10 min-h-[250px] lg:min-h-[600px]">
+        <div id="callout2-container" className="gradient-4 z-10 md:rounded-tl-[5vmax] relative flex flex-col items-end lg:items-start justify-end pt-0 lg:pt-5 p-5 pb-[10vmax] md:pr-10 md:pl-10 min-h-[250px] lg:min-h-[600px]">
           <div id="background-elements"> 
               <div className="gradient-3 w-full h-[50%] absolute bottom-0 left-0"></div>
               <div id="bottom-divider" className="hidden divider-4 absolute left-0 bottom-[-2px] w-[60%] h-full"></div>
-              <div id="featured-image" className="feature-3 absolute top-0 right-0 w-[80vmin] md:w-[70%] lg:w-[45vmax] mt-[-5vmax] h-full mt-5"></div>
+              <div id="featured-image" className="hide-xl feature-3 absolute top-0 right-0 w-[80vmin] md:w-[70%] lg:w-[45vmax] mt-[-5vmax] h-full mt-5"></div>
+              <div id="featured-image" className="show-xl feature-3 absolute top-0 right-0 w-[900px] h-[50vw]"></div>
           </div>
-          <div id="content" className="relative pt-[220px] md:pt-20 md:pb-0 w-full pr-5 lg:w-[70%]">
+          <div id="content" className="relative pt-[220px] lg:pt-20 md:pb-0 w-full pr-5 lg:w-[70%]">
               <h1 className="header font-GoodTimes">
                 Space is for<br></br>EVERYONE 
               </h1>
@@ -21,9 +21,9 @@ export default function Callout2() {
               borderRadius="rounded-tl-[10px] rounded-[2vmax]"
               link="https://app.moondao.com/join"
               paddingOnHover="pl-5"
-            >
-              Become A Member
-            </StandardButton>
+              >
+                Become A Member
+              </StandardButton>
           </div>
         </div>
     </section>
