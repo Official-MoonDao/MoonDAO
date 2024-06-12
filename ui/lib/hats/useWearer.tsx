c450f10a99a29aa92e6753ba7f62061a12ec976a
colinmfoster4723 <colin.foster4723@gmail.com>
2024-06-12 09:49:07 -0500

    Added filter for moondao hat tree

--- ui/lib/hats/useWearer.tsx
+++ ui/lib/hats/useWearer.tsx
@@ -1,9 +1,10 @@
 import { Chain } from '@thirdweb-dev/chains'
+import { MOONDAO_HAT_TREE_IDS } from 'const/config'
 import { useEffect, useState } from 'react'
 import hatsSubgraphClient from './hatsSubgraphClient'
 
 export function useWearer(selectedChain: Chain, address: any) {
-  const [hats, setHats] = useState<any>([])
+  const [wornMoondaoHats, setWornMoondaoHats] = useState<any>([])
 
   async function getWearerHats() {
     try {
@@ -13,11 +14,20 @@ export function useWearer(selectedChain: Chain, address: any) {
         wearerAddress: address,
         props: {
           currentHats: {
-            props: {},
+            props: {
+              tree: {},
+            },
           },
         },
       })
-      setHats(hats?.currentHats)
+
+      //filter worn hats to only include hats that are in the MoonDAO hat tree
+      if (hats.currentHats) {
+        const filteredHats = hats.currentHats.filter(
+          (hat: any) => hat.tree.id === MOONDAO_HAT_TREE_IDS[selectedChain.slug]
+        )
+        setWornMoondaoHats(filteredHats)
+      }
     } catch (err) {
       console.log(err)
     }
@@ -27,5 +37,5 @@ export function useWearer(selectedChain: Chain, address: any) {
     getWearerHats()
   }, [selectedChain, address])
 
-  return hats
+  return wornMoondaoHats
 }
