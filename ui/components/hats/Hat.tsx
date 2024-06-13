d543e4f92d73737096b2c2e667dec1c2935df4b6
colinmfoster4723 <colin.foster4723@gmail.com>
2024-06-13 18:14:47 -0500

    Adjusted hat link

--- ui/components/hats/Hat.tsx
+++ ui/components/hats/Hat.tsx
@@ -1,26 +1,23 @@
 import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
 import { Chain } from '@thirdweb-dev/chains'
+import { MOONDAO_HAT_TREE_IDS } from 'const/config'
 import Link from 'next/link'
-import { useEffect } from 'react'
 import { useHatData } from '@/lib/hats/useHatData'
-import { useHandleRead } from '@/lib/thirdweb/hooks'
 
 type HatProps = {
   selectedChain: Chain
   hatsContract: any
-  hatId: string
+  hatId: any
 }
 
 export function Hat({ selectedChain, hatsContract, hatId }: HatProps) {
-  const hatData = useHatData(hatsContract, hatId)
-
-  const { data: treeId } = useHandleRead(hatsContract, 'getTopHatDomain', [
-    hatId,
-  ])
+  const hatData = useHatData(selectedChain, hatsContract, hatId)
 
   return (
     <Link
-      href={`https://app.hatsprotocol.xyz/trees/${selectedChain.chainId}/${treeId}?hatId=${hatId}`}
+      href={`https://app.hatsprotocol.xyz/trees/${selectedChain.chainId}/${
+        MOONDAO_HAT_TREE_IDS[selectedChain.slug]
+      }?hatId=${hatData.prettyId}`}
       className="px-4 flex flex-col"
       target="_blank"
       rel="noreferrer"
