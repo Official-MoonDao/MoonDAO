22a9c124153d452bc75b9064f08bd6982e8a3557
colinmfoster4723 <colin.foster4723@gmail.com>
2024-06-13 18:20:55 -0500

    Added pretty id to hat data

--- ui/lib/hats/useHatData.tsx
+++ ui/lib/hats/useHatData.tsx
@@ -1,15 +1,17 @@
 import { useResolvedMediaType } from '@thirdweb-dev/react'
 import { useEffect, useState } from 'react'
+import hatsSubgraphClient from './hatsSubgraphClient'
 
-export function useHatData(hatsContract: any, hatId: string) {
+export function useHatData(selectedChain: any, hatsContract: any, hatId: any) {
   const [hat, setHat] = useState<any>({})
   const [hatMetadataURI, setHatMetadataURI] = useState('')
   const resolvedMetadata = useResolvedMediaType(hatMetadataURI)
   const [hatData, setHatData] = useState({
-    name: 'Undefined',
+    name: '',
     description: '',
     supply: 0,
     active: null,
+    prettyId: '',
   })
 
   async function getHatAndMetadata() {
@@ -18,17 +20,37 @@ export function useHatData(hatsContract: any, hatId: string) {
     setHatMetadataURI(hat.details)
   }
 
+  async function getHatPrettyId() {
+    const hat: any = await hatsSubgraphClient.getHat({
+      chainId: selectedChain.chainId,
+      hatId,
+      props: {
+        prettyId: true,
+      },
+    })
+  }
+
   async function getHatData() {
     const { supply, active } = hat
 
     try {
       const metadataRes = await fetch(resolvedMetadata.url)
       const { data } = await metadataRes.json()
+
+      const hatSubgraphData = await hatsSubgraphClient.getHat({
+        chainId: selectedChain.chainId,
+        hatId,
+        props: {
+          prettyId: true,
+        },
+      })
+
       setHatData({
         name: data.name,
         description: data.description,
         supply: supply,
         active: active,
+        prettyId: hatSubgraphData?.prettyId?.toString() as string,
       })
     } catch (err: any) {
       console.error(err)
