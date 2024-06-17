8755f248229f3d4c5ac091bacfeb6ae189134cc7
colinmfoster4723 <colin.foster4723@gmail.com>
2024-06-17 16:24:49 -0500

    Added data type check

--- ui/lib/tableland/isTextValid.ts
+++ ui/lib/tableland/isTextValid.ts
@@ -1,13 +1,17 @@
 import toast from 'react-hot-toast'
 
 export default function isTextInavlid(text: string) {
+  if (!text || text === '' || typeof text !== 'string') {
+    return false
+  }
+
   if (/\p{Extended_Pictographic}/u.test(text)) {
     toast.error('Emojis are not allowed.', { duration: 10000 })
     return true
   }
 
   //check if text includes a single quote
-  if (text.includes("'")) {
+  if (text?.includes("'")) {
     toast.error('Single quotes are not allowed.', {
       duration: 10000,
     })
