3bae0e635a346e6f8b6c2448f5fcf6af82af868c
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-06-26 23:00:44 -0400

    Subscription component updates

--- ui/components/subscription/Card.tsx
+++ ui/components/subscription/Card.tsx
@@ -1,10 +1,15 @@
+import Frame from "../layout/Frame"
+
 export default function Card({ children, className = '', onClick }: any) {
   return (
     <div
-      className={`p-8 md:p-4 rounded-md dark:bg-[#080C20] border-2 dark:border-0 text-start text-black dark:text-white ${className}`}
       onClick={onClick}
     >
-      {children}
+      <Frame 
+        noPadding
+        >
+        {children}
+      </Frame>
     </div>
   )
 }
--- ui/components/subscription/EntityDonation.tsx
+++ ui/components/subscription/EntityDonation.tsx
@@ -7,6 +7,7 @@ import PrivyWalletContext from '@/lib/privy/privy-wallet-context'
 import { useNativeBalance } from '@/lib/thirdweb/hooks/useNativeBalance'
 import Button from './Button'
 import Card from './Card'
+import Frame from '../layout/Frame'
 
 type EntityDonationProps = {
   splitAddress: string | undefined
@@ -39,25 +40,31 @@ export default function EntityDonation({ splitAddress }: EntityDonationProps) {
   return (
     <Card className="flex flex-col lg:flex-row justify-between gap-4">
       <p className="text-2xl">Contribute</p>
-      <div className="flex flex-col lg:flex-row gap-4">
-        <form
-          className="flex gap-4 items-center"
-          onSubmit={(e) => {
-            e.preventDefault()
-            donate()
-          }}
-        >
-          <input
-            className="w-[100px] bg-[#0f152f] px-2 h-full"
-            type="number"
-            onChange={({ target }: any) => setDonationAmount(target.value)}
-            value={donationAmount}
-            step={0.001}
-          />
-          <p>ETH</p>
-          <Button type="submit">Donate</Button>
-        </form>
-      </div>
+        <div className="flex flex-col lg:flex-row gap-4">
+        <Frame noPadding>
+          <form
+            className="flex bg-dark-cool gap-4 items-center justify-between"
+            onSubmit={(e) => {
+              e.preventDefault()
+              donate()
+            }}
+          >
+            <input
+              className="w-[100px] bg-dark-cool pl-5 h-full"
+              type="number"
+              onChange={({ target }: any) => setDonationAmount(target.value)}
+              value={donationAmount}
+              step={0.001}
+            />
+            <p>ETH</p>
+            <div id=""
+              className="gradient-2"
+              >
+              <Button type="submit">Donate</Button>
+            </div>
+          </form>
+          </Frame>
+        </div>  
     </Card>
   )
 }
