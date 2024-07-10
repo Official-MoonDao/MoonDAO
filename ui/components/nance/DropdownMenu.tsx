7f1fb6e7ae9603a02073219b4a5a7194f60129ee
twodam <twodam@protonmail.com>
2024-07-10 15:44:27 +0800

    fix(DropdownMenu): only one MenuButton can used to open/close the menu

--- ui/components/nance/DropdownMenu.tsx
+++ ui/components/nance/DropdownMenu.tsx
@@ -110,22 +110,22 @@ export default function DropDownMenu({
   return (
     <>
       <Menu as="div" className="relative inline-block">
-        <div>
-          <MenuButton className="inline-flex w-full justify-end rounded-md sm:hidden">
+        <MenuButton>
+          <div className="inline-flex w-full justify-end rounded-md sm:hidden">
             <EllipsisVerticalIcon
-              className="h-7 w-7 text-indigo-600 hover:text-gray-900"
+              className="h-7 w-7 text-indigo-600"
               aria-hidden="true"
             />
-          </MenuButton>
+          </div>
 
-          <MenuButton className="hidden w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:inline-flex">
+          <div className="hidden w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:inline-flex">
             Options
             <ChevronDownIcon
               className="-mr-1 h-5 w-5 text-gray-400"
               aria-hidden="true"
             />
-          </MenuButton>
-        </div>
+          </div>
+        </MenuButton>
         <Transition
           as={Fragment}
           enter="transition ease-out duration-100"
