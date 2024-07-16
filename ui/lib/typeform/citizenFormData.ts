498b34d1c3b8c3d69187c65e56be1a47880d0570
colinmfoster4723 <colin.foster4723@gmail.com>
2024-07-15 19:09:06 -0500

    Updated field ids for typeform data

--- ui/lib/typeform/citizenFormData.ts
+++ ui/lib/typeform/citizenFormData.ts
@@ -18,15 +18,15 @@ export default function formatCitizenFormData(
   responseId: string
 ) {
   return {
-    name: answerByFieldId(answers, 'Zj6QbNhOey3i').text,
-    email: answerByFieldId(answers, 'ggrOjApkLFMz').email,
+    name: answerByFieldId(answers, 'RkBAEVN40hLw').text,
+    email: answerByFieldId(answers, 'LzGGOX3e8Sfv').email,
     description: answerByFieldId(answers, 'PtIcC6l6F5bl')?.text || '',
     discord: answerByFieldId(answers, 'WzZ35V8MLS4J')?.text || '',
     website: answerByFieldId(answers, 'RXW5Ij2CH5g3')?.url || '',
     twitter: answerByFieldId(answers, 'oHfMqgeSg3sa')?.url || '',
     location: answerByFieldId(answers, 'pP6s24aIwEl7')?.text || '',
     view:
-      answerByFieldId(answers, 'vv8LGLkyzIaO').choice.label === 'Yes'
+      answerByFieldId(answers, 'W0uayCnonUN7').boolean === true
         ? 'public'
         : 'private',
     newsletterSub: answerByFieldId(answers, 'EnRjgahQqOFG')?.boolean,
--- ui/lib/typeform/teamFormData.ts
+++ ui/lib/typeform/teamFormData.ts
@@ -10,18 +10,16 @@ export type TeamData = {
   formResponseId: string
 }
 
-export default function formatTeamFormData(
-  answers: any[],
-  responseId: string
-) {
+export default function formatTeamFormData(answers: any[], responseId: string) {
+  console.log(answers)
   return {
-    name: answerByFieldId(answers, 'aYPTOtKo0en0').text,
-    description: answerByFieldId(answers, 'TGICeck61q0r')?.text || '',
-    twitter: answerByFieldId(answers, 'ti5k7RkHB6kD')?.url || '',
-    communications: answerByFieldId(answers, '0TKNKT8IKsqN')?.url || '',
-    website: answerByFieldId(answers, 'Nx3JEhTiY2se')?.url || '',
+    name: answerByFieldId(answers, 'ODbjYccwMg1E').text,
+    description: answerByFieldId(answers, 'Cg2Wv2FOW3wN')?.text || '',
+    twitter: answerByFieldId(answers, 'gpyKF4T4oSsH')?.url || '',
+    communications: answerByFieldId(answers, '3cGtidMEK0LA')?.url || '',
+    website: answerByFieldId(answers, 'ypWYK8jHAPXf')?.url || '',
     view:
-      answerByFieldId(answers, 'fOKwAkJnJuQH').choice.label === 'Yes'
+      answerByFieldId(answers, 'MfYME9ERpJR6').boolean === true
         ? 'public'
         : 'private',
     formResponseId: responseId,
