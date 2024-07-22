6b7a6696aa25fe5ecbb09ac7e5f1c01d5eb99e22
colinmfoster4723 <colin.foster4723@gmail.com>
2024-07-22 15:21:10 -0500

    Updated moondao hat tree id

--- ui/const/config.ts
+++ ui/const/config.ts
@@ -118,7 +118,7 @@ export const TEAM_DISCOUNTLIST_ADDRESSES: Index = {
 }
 
 export const MOONDAO_HAT_TREE_IDS: Index = {
-  arbitrum: '0x00000024',
+  arbitrum: '0x00000026',
   sepolia: '0x0000017c',
 }
 
