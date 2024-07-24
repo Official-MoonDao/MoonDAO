e0ee7276e0dfd5b857451e217ed5ac3172deb8bb
colinmfoster4723 <colin.foster4723@gmail.com>
2024-07-24 11:28:20 -0500

    Refactor

--- ui/pages/team/[tokenId].tsx
+++ ui/pages/team/[tokenId].tsx
@@ -154,7 +154,7 @@ export default function TeamDetailPage({ tokenId, nft, imageIpfsLink }: any) {
 
   useEffect(() => {
     setSelectedChain(
-      process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? Arbitrum : Sepolia comm
+      process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? Arbitrum : Sepolia
     )
   }, [])
 
