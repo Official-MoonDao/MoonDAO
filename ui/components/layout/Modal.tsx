6894ac4efdad5a8520a0ace133b4ee5249be9d22
Mitchie <88346127+MitchMcQuinn@users.noreply.github.com>
2024-09-10 20:05:12 -0400

    Update to the manage members modal
    - Resolved issue where nav elements were appearing overtop of the modal despite having a lower z-index by using a portal to render the modal directly as a child of the body element.
    - Button only clickable when valid address is entered
    - Updated overall aesthetic (colors, icons, spacing and positioning, rounding, etc...)

--- ui/components/layout/Modal.tsx
+++ ui/components/layout/Modal.tsx
@@ -1,19 +1,28 @@
+import { createPortal } from 'react-dom'
+
 type ModalProps = {
   id: string
   setEnabled: Function
   children: any
 }
 
+function Portal({ children }: { children: React.ReactNode }) {
+  if (typeof document === 'undefined') return null // SSR check
+  return createPortal(children, document.body)
+}
+
 export default function Modal({ id, setEnabled, children }: ModalProps) {
   return (
-    <div
-      onMouseDown={(e: any) => {
-        if (e.target.id === id) setEnabled(false)
-      }}
-      id={id}
-      className="fixed top-0 left-0 w-screen h-screen bg-[#00000080] backdrop-blur-sm flex justify-center items-center z-[2000] overflow-auto"
-    >
-      {children}
-    </div>
+    <Portal>
+      <div
+        onMouseDown={(e: any) => {
+          if (e.target.id === id) setEnabled(false)
+        }}
+        id={id}
+        className="fixed top-0 left-0 w-screen h-screen bg-[#00000080] backdrop-blur-sm flex justify-center items-center z-[2000] overflow-auto"
+      >
+        {children}
+      </div>
+    </Portal>
   )
 }
