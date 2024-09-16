584662fb49484f0333e9eae86b266e342940b6fa
name.get <99998750+namedotget@users.noreply.github.com>
2024-09-16 18:33:02 -0500

    Cleared citizen blocklist and updated citizen addresses (#190)
    * Adjusted links and images
    
    * Refactor, adjusted useEffect deps
    
    * Refactor
    
    * Added image alt
    
    * Adjusted gtag and added cookie banner
    
    * Removed unused script
    
    * Adjusted styling
    
    * Refactor
    
    * Refactor
    
    * Removed old citizen nft from flow
    
    * Refactor
    
    * Added image domain
    
    * Refactor
    
    * Adjusted network mismatch
    
    * Refactor and adjusted fetching of citizens and teams
    
    * Configured citizen and team blocklists
    
    * Added link to blocklist spreadsheet
    
    * Adjusted styling
    
    * Disabled citizen actions if sub isn't valid
    
    * Adjusted max page and refactor
    
    * Added funding condition to checkout
    
    * Added privy auth check, set citizen nft as undefined if not logged in
    
    * Refactor
    
    * Adjusted styling, consolidated team members
    
    * Removed unused files
    
    * Added onClick prop to index card
    
    * Removed unused imports
    
    * Cleared citizen blocklist
    
    * Updated citizen addresses

--- ui/const/whitelist.ts
+++ ui/const/whitelist.ts
@@ -1,5 +1,5 @@
 //https://docs.google.com/spreadsheets/d/1LsYxkI_1alFUD_NxM2a5RngeSRC5e5ElUpP6aR30DiM/edit?gid=0#gid=0
 export const blockedTeams: any = []
-export const blockedCitizens: any = [4]
+export const blockedCitizens: any = []
 
 export const featuredEntities: any = [1]
