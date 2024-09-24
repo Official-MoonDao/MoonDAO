40a67653ea3e4908b1da7b9077ca8a675e5cbaf4
colinmfoster4723 <colin.foster4723@gmail.com>
2024-09-24 10:55:51 -0500

    Added swap to get-mooney

--- ui/components/uniswap/NativeToMooney.tsx
+++ ui/components/uniswap/NativeToMooney.tsx
@@ -1,25 +1,15 @@
-import {
-  ArrowDownIcon,
-  InformationCircleIcon,
-} from '@heroicons/react/24/outline'
+import { ArrowDownIcon } from '@heroicons/react/24/outline'
 import { useAddress } from '@thirdweb-dev/react'
 import { TradeType } from '@uniswap/sdk-core'
-import { Token } from '@uniswap/sdk-core'
-import { SwapRoute, nativeOnChain } from '@uniswap/smart-order-router'
+import { SwapRoute } from '@uniswap/smart-order-router'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useUniversalRouter } from '../../lib/uniswap/hooks/useUniversalRouter'
 import { useNativeBalance } from '@/lib/thirdweb/hooks/useNativeBalance'
-import { MOONEY_ADDRESSES } from '../../const/config'
+import { useUniswapTokens } from '@/lib/uniswap/hooks/useUniswapTokens'
 import GasIcon from '../assets/GasIcon'
 import { PrivyWeb3Button } from '../privy/PrivyWeb3Button'
-
-const swapMinimums: any = {
-  MOONEY: '10,000',
-  MATIC: '0.000001',
-  ETH: '0.0025',
-}
 
 const nativeSymbols: any = {
   ethereum: 'ETH',
@@ -41,24 +31,12 @@ export default function NativeToMooney({ selectedChain }: any) {
   const [swapRoute, setSwapRoute] = useState<SwapRoute>()
   const [estimatedGasUsedUSD, setEstimatedGasUsedUSD] = useState<any>(0)
 
-  useEffect(() => {
-    const native = nativeOnChain(selectedChain.chainId)
-    const mooney = new Token(
-      selectedChain.chainId,
-      MOONEY_ADDRESSES[selectedChain.slug],
-      18,
-      'MOONEY',
-      'MOONEY'
-    )
-
-    setInputToken(native)
-    setOutputToken(mooney)
-  }, [selectedChain])
+  const { NATIVE, MOONEY } = useUniswapTokens(selectedChain)
 
   const { generateRoute, executeRoute } = useUniversalRouter(
     amount,
-    inputToken,
-    outputToken
+    NATIVE,
+    MOONEY
   )
 
   useEffect(() => {
@@ -76,16 +54,17 @@ export default function NativeToMooney({ selectedChain }: any) {
         }
       })
     }
-  }, [amount, selectedChain, inputToken, address])
+  }, [amount, selectedChain, inputToken, outputToken, address])
 
   return (
     <div className="max-w-[500px] w-full flex flex-col gap-1">
-      <div className="flex flex-col p-4 gap-2 bg-[#120f21] min-h-[150px] rounded-lg">
+      <div className="flex flex-col p-4 gap-2 bg-darkest-cool min-h-[150px] rounded-lg">
         <p className="opacity-50">You Pay</p>
         <div className="flex justify-between">
           <input
             className="text-white bg-transparent md:text-2xl"
             placeholder="Amount"
+            pattern="[0-9]*[.,]?[0-9]*"
             onChange={({ target }) => {
               setAmount(Number(target.value))
             }}
@@ -101,18 +80,23 @@ export default function NativeToMooney({ selectedChain }: any) {
             <p>{nativeSymbols[selectedChain?.slug]}</p>
           </div>
         </div>
-        <p className="opacity-50">{`Minimum: ${
-          swapMinimums[inputToken?.symbol]
-        } ${inputToken?.symbol}`}</p>
         {address && <p className="opacity-50">{`Balance: ${nativeBalance}`}</p>}
       </div>
-      <div className="h-0 w-full flex justify-center items-center z-10">
-        <div className="p-4 bg-[#29253f] rounded-full">
+      <div className="h-0 w-full flex justify-center items-center z-[5]">
+        <button
+          className="p-4 bg-[#29253f] rounded-full"
+          onClick={() => {
+            if (swapRoute)
+              setAmount(+swapRoute.route[0].rawQuote.toString() / 10 ** 18 || 0)
+            setInputToken(outputToken)
+            setOutputToken(inputToken)
+          }}
+        >
           <ArrowDownIcon width={25} height={25} />
-        </div>
+        </button>
       </div>
 
-      <div className="flex flex-col p-4 gap-2 bg-[#120f21] min-h-[150px] rounded-lg">
+      <div className="flex flex-col p-4 gap-2 bg-darkest-cool min-h-[150px] rounded-lg">
         <p className="opacity-50">You Receive</p>
         <div className="flex justify-between items-center">
           <p className="md:text-2xl">{output?.toLocaleString() || 0}</p>
@@ -123,32 +107,37 @@ export default function NativeToMooney({ selectedChain }: any) {
         </div>
       </div>
 
-      <div className="mt-2 p-2 w-full flex justify-between border-[1px] rounded-lg">
-        <p>Network Cost</p>
-        <div className="flex gap-2 items-center">
-          <GasIcon />
-          <p>{`$ ${estimatedGasUsedUSD}`}</p>
-        </div>
-      </div>
-
-      <div className="mt-2 p-2 w-full flex justify-between border-[1px] border-moon-orange rounded-lg text-moon-orange">
-        <p>Price Impact</p>
-        <p>{`~ ${swapRoute ? swapRoute.trade.priceImpact.toFixed(2) : 0}%`}</p>
-      </div>
-
-      {selectedChain.slug === 'arbitrum' && (
-        <div className="mt-2 p-2 w-full flex justify-between border-[1px] border-moon-orange rounded-lg text-moon-orange text-[75%]">
+      <div className="flex flex-col bg-darkest-cool p-4">
+        <div className="mt-2 p-2 w-full flex justify-between border-[1px] rounded-lg">
+          <p>Network Cost</p>
           <div className="flex gap-2 items-center">
-            <p>{`Please note that due to low liquidity in the Arbitrum L2 pool, swapping ETH for $MOONEY may result in receiving fewer $MOONEY than expected. We recommend reviewing current liquidity conditions and potential slippage before proceeding with your swap.
-`}</p>
+            <GasIcon />
+            <p>{`$ ${estimatedGasUsedUSD}`}</p>
           </div>
         </div>
-      )}
+
+        <div className="mt-2 p-2 w-full flex justify-between border-[1px] border-moon-orange rounded-lg text-moon-orange">
+          <p>Price Impact</p>
+          <p>{`~ ${
+            swapRoute ? swapRoute.trade.priceImpact.toFixed(2) : 0
+          }%`}</p>
+        </div>
+
+        {selectedChain.slug === 'arbitrum' && (
+          <div className="mt-2 p-2 w-full flex justify-between border-[1px] border-moon-orange rounded-lg text-moon-orange text-[75%]">
+            <div className="flex gap-2 items-center">
+              <p>{`Please note that due to low liquidity in the Arbitrum L2 pool, swapping ETH for $MOONEY may result in receiving fewer $MOONEY than expected. We recommend reviewing current liquidity conditions and potential slippage before proceeding with your swap.
+`}</p>
+            </div>
+          </div>
+        )}
+      </div>
 
       <PrivyWeb3Button
-        className="bg-moon-orange mt-2"
+        className="mt-2 rounded-[5vmax] rounded-tl-[20px]"
         label={'Swap'}
         action={async () => {
+          if (amount === 0) return toast.error('Enter an amount')
           if (!swapRoute) return toast.error('No route found')
 
           // check native balance
--- ui/pages/get-mooney.tsx
+++ ui/pages/get-mooney.tsx
@@ -1,38 +1,21 @@
-import React, { useContext, useEffect } from 'react'
-import { Ethereum, Polygon, Sepolia } from '@thirdweb-dev/chains'
+import { Polygon } from '@thirdweb-dev/chains'
 import { Token } from '@uniswap/sdk-core'
 import useTranslation from 'next-translate/useTranslation'
+import React, { useContext } from 'react'
 import { pregenSwapRoute } from '../lib/uniswap/pregenSwapRoute'
-import { OnboardingStageManager } from '../components/get-mooney/OnboardingStageManager'
-import WebsiteHead from '../components/layout/Head'
-import { DAI_ADDRESSES, MOONEY_ADDRESSES } from '../const/config'
 import ChainContext from '@/lib/thirdweb/chain-context'
 import Container from '../components/layout/Container'
 import ContentLayout from '../components/layout/ContentLayout'
+import WebsiteHead from '../components/layout/Head'
 import { NoticeFooter } from '@/components/layout/NoticeFooter'
-
-function JoinCard({ label, text }: any) {
-  return (
-    <div className="flex">
-      <div>
-        <dt className="text-lg font-RobotoMono font-medium text-left lg:text-left text-gray-950 dark:text-white">
-          {label}
-        </dt>
-        <dd className="mt-5 lg:mt-4 xl:mt-6 text-sm sm:text-base lg:text-sm xl:text-base sm:mt-6 max-w-[698px] text-left lg:text-left text-gray-600 dark:text-white dark:opacity-60">
-          {text}
-        </dd>
-      </div>
-    </div>
-  )
-}
+import NetworkSelector from '@/components/thirdweb/NetworkSelector'
+import NativeToMooney from '@/components/uniswap/NativeToMooney'
+import { OnboardingStageManager } from '../archive/get-mooney/OnboardingStageManager'
+import { DAI_ADDRESSES, MOONEY_ADDRESSES } from '../const/config'
 
 export default function Join({ usdQuotes }: any) {
   const { t } = useTranslation('common')
-  const { selectedChain, setSelectedChain } = useContext(ChainContext)
-
-  useEffect(() => {
-    setSelectedChain(process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? Ethereum : Sepolia)
-  }, [])
+  const { selectedChain } = useContext(ChainContext)
 
   return (
     <>
@@ -49,9 +32,8 @@ export default function Join({ usdQuotes }: any) {
             mode="compact"
             popOverEffect={false}
           >
-            <div className="mb-10 flex justify-center">
-              <OnboardingStageManager usdQuotes={usdQuotes} />
-            </div>
+            <NetworkSelector />
+            <NativeToMooney selectedChain={selectedChain} />
           </ContentLayout>
         </Container>
       </section>
@@ -59,35 +41,35 @@ export default function Join({ usdQuotes }: any) {
   )
 }
 
-export async function getStaticProps() {
-  const DAI = new Token(
-    137,
-    DAI_ADDRESSES['polygon'],
-    18,
-    'DAI',
-    'DAI Stablecoin'
-  )
+// export async function getStaticProps() {
+//   const DAI = new Token(
+//     137,
+//     DAI_ADDRESSES['polygon'],
+//     18,
+//     'DAI',
+//     'DAI Stablecoin'
+//   )
 
-  const MOONEY = new Token(
-    137,
-    MOONEY_ADDRESSES['polygon'],
-    18,
-    'MOONEY',
-    'MOONEY (PoS)'
-  )
+//   const MOONEY = new Token(
+//     137,
+//     MOONEY_ADDRESSES['polygon'],
+//     18,
+//     'MOONEY',
+//     'MOONEY (PoS)'
+//   )
 
-  const levelOneRoute = await pregenSwapRoute(Polygon, 20000, MOONEY, DAI)
-  const levelTwoRoute = await pregenSwapRoute(Polygon, 100000, MOONEY, DAI)
-  const levelThreeRoute = await pregenSwapRoute(Polygon, 500000, MOONEY, DAI)
+//   const levelOneRoute = await pregenSwapRoute(Polygon, 20000, MOONEY, DAI)
+//   const levelTwoRoute = await pregenSwapRoute(Polygon, 100000, MOONEY, DAI)
+//   const levelThreeRoute = await pregenSwapRoute(Polygon, 500000, MOONEY, DAI)
 
-  const usdQuotes = [levelOneRoute, levelTwoRoute, levelThreeRoute].map(
-    (swapRoute) => swapRoute?.route[0].rawQuote.toString() / 10 ** 18
-  )
+//   const usdQuotes = [levelOneRoute, levelTwoRoute, levelThreeRoute].map(
+//     (swapRoute) => swapRoute?.route[0].rawQuote.toString() / 10 ** 18
+//   )
 
-  return {
-    props: {
-      usdQuotes,
-    },
-    revalidate: 60,
-  }
-}
+//   return {
+//     props: {
+//       usdQuotes,
+//     },
+//     revalidate: 60,
+//   }
+// }
