4f13ba461796d000395efc747faeb3216b8d4592
colinmfoster4723 <colin.foster4723@gmail.com>
2024-10-24 11:55:03 -0500

    Adjusted gas limit

--- ui/components/onboarding/CreateCitizen.tsx
+++ ui/components/onboarding/CreateCitizen.tsx
@@ -489,6 +489,7 @@ export default function CreateCitizen({
                         ],
                         {
                           value: cost,
+                          gasLimit: 1000000,
                         }
                       )
 
