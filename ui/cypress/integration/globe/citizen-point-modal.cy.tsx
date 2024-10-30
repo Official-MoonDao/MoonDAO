80728c7f145638964edb2b055a17434d0a409291
colinmfoster4723 <colin.foster4723@gmail.com>
2024-10-30 15:14:26 -0500

    Added globe tests

---
+++ ui/cypress/integration/globe/citizen-point-modal.cy.tsx
@@ -0,0 +1,50 @@
+import CitizenPointModal from '@/components/globe/CitizenPointModal'
+
+describe('<CitizenPointModal />', () => {
+  let props: any
+
+  beforeEach(() => {
+    props = {
+      selectedPoint: {
+        formattedAddress: '123 Main St, Anytown, USA',
+        citizens: [
+          {
+            id: 1,
+            name: 'Ryan',
+            image:
+              'https://b507f59d2508ebfb5e70996008095782.ipfscdn.io/ipfs/bafybeifh2vwvfxfy6fevqkirldplgp47sfblcfvhn7nsxo4z4krsuulf2e/',
+          },
+          {
+            id: 2,
+            name: 'Name.get',
+            image:
+              'https://b507f59d2508ebfb5e70996008095782.ipfscdn.io/ipfs/bafybeibo5na6nkatvor7bqisybzwtmh5n4l4wuws3uiyoqvjuuqwzwobna/',
+          },
+        ],
+      },
+      setEnabled: cy.stub(),
+    }
+
+    cy.mount(<CitizenPointModal {...props} />)
+  })
+
+  it('Renders the formatted address', () => {
+    cy.contains('123 Main St, Anytown, USA').should('exist')
+  })
+
+  it('Renders citizen images and names', () => {
+    props.selectedPoint.citizens.forEach((citizen: any) => {
+      cy.get(`img[alt="${citizen.name}"]`).should(
+        'have.attr',
+        'src',
+        citizen.image
+      )
+      cy.contains(citizen.name).should('exist')
+    })
+  })
+
+  it('Closes the modal when the close button is clicked', () => {
+    cy.get('#close-modal').click()
+    cy.wrap(props.setEnabled).should('have.been.calledWith', false)
+  })
+})
