55f7890dc86064124d8486ac90508b7f5839dc89
colinmfoster4723 <colin.foster4723@gmail.com>
2024-10-31 15:32:19 -0500

    Adjusted globe size

--- ui/lib/globe/useGlobeSize.tsx
+++ ui/lib/globe/useGlobeSize.tsx
@@ -2,17 +2,25 @@ import { useEffect, useState } from 'react'
 
 export default function useGlobeSize() {
   const [size, setSize] = useState({ width: 500, height: 500 })
+
   useEffect(() => {
-    let width, height
-    if (window?.innerWidth > 1500) {
-      width = 1000
-    } else if (window?.innerWidth > 500) {
-      width = window.innerWidth * 0.6
-    } else {
-      width = window.innerWidth * 0.9
+    function getSize() {
+      let width
+      if (window.innerWidth > 1500) {
+        width = 1000
+      } else if (window.innerWidth > 500) {
+        width = window.innerWidth * 0.6
+      } else {
+        width = window.innerWidth * 0.9
+      }
+      setSize({ width, height: width })
     }
-    height = width
-    setSize({ width, height })
+    getSize()
+
+    window.addEventListener('resize', getSize)
+
+    return () => window.removeEventListener('resize', getSize)
   }, [])
+
   return size
 }
