ded08df454952491d355308fe17c0aa83e041ccf
colinmfoster4723 <colin.foster4723@gmail.com>
2024-11-06 11:42:26 -0600

    Added attributes check

--- ui/lib/utils/nft.ts
+++ ui/lib/utils/nft.ts
@@ -1,4 +1,5 @@
 export function getAttribute(attributes: any[], traitType: string) {
+  if (!attributes) return null
   return Object.values(attributes).find(
     (attr: any) => attr.trait_type === traitType
   )
