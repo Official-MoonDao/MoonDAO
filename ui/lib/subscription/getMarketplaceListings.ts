4c24e354ca6d9fd49af8cf5a52f3d762605d076e
name.get <99998750+namedotget@users.noreply.github.com>
2024-11-11 12:17:01 -0600

    Added marketplace listing non-citizen upcharge (#244)
    * Added non-citizen marketplace upcharge
    
    * Added markup message
    
    * Refactor
    
    * Adjusted styling and added token value truncation
    
    * Rounded mooney value
    
    * Added citizenship field
    
    * Added original value check
    
    * Added test for marketplace
    
    * Added ids for testing
    
    * Added mock prop
    
    * Added tests for marketplace

---
+++ ui/lib/subscription/getMarketplaceListings.ts
@@ -0,0 +1,38 @@
+import { Arbitrum, Sepolia } from '@thirdweb-dev/chains'
+import {
+  MARKETPLACE_TABLE_ADDRESSES,
+  TABLELAND_ENDPOINT,
+  TEAM_ADDRESSES,
+} from 'const/config'
+import { initSDK } from '../thirdweb/thirdweb'
+
+export default async function getMarketplaceListings() {
+  const chain = process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? Arbitrum : Sepolia
+  const sdk = initSDK(chain)
+  const marketplaceTableContract = await sdk.getContract(
+    MARKETPLACE_TABLE_ADDRESSES[chain.slug]
+  )
+  const teamContract = await sdk.getContract(TEAM_ADDRESSES[chain.slug])
+
+  const marketplaceTableName = await marketplaceTableContract.call(
+    'getTableName'
+  )
+
+  const statement = `SELECT * FROM ${marketplaceTableName}`
+
+  const allListingsRes = await fetch(
+    `${TABLELAND_ENDPOINT}?statement=${statement}`
+  )
+  const allListings = await allListingsRes.json()
+
+  const now = Math.floor(Date.now() / 1000)
+
+  const validListings = allListings.filter(async (listing: any) => {
+    const teamExpiration = await teamContract.call('expiresAt', [
+      listing.teamId,
+    ])
+    return teamExpiration.toNumber() > now
+  })
+
+  return validListings
+}
