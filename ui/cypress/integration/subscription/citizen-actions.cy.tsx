a702d3b3c02c2a6c793d8cffb39c60c8f6bf0bd9
colinmfoster4723 <colin.foster4723@gmail.com>
2024-11-18 18:33:48 -0600

    Updated tests

--- ui/cypress/integration/subscription/citizen-actions.cy.tsx
+++ ui/cypress/integration/subscription/citizen-actions.cy.tsx
@@ -4,13 +4,10 @@ import CitizenActions from '@/components/subscription/CitizenActions'
 describe('<CitizenActions />', () => {
   let props: any
   let nft: any
-  let incompleteNft: any
+
   before(() => {
     cy.fixture('nft/citizen-nft').then((n) => {
       nft = n
-    })
-    cy.fixture('nft/incomplete-citizen-nft').then((n) => {
-      incompleteNft = n
     })
   })
 
@@ -21,18 +18,29 @@ describe('<CitizenActions />', () => {
     }
 
     cy.mountNextRouter('/')
-    cy.mount(<CitizenActions {...props} />)
+    cy.mount(<CitizenActions {...props} mooneyBalance={0} vmooneyBalance={0} />)
   })
 
   it('Renders component', () => {
-    cy.get('div#team-actions-container').should('exist')
+    cy.get('#citizen-actions-container').should('exist')
     cy.contains('Create Project').should('exist')
-    cy.contains('Browse Jobs').should('exist')
+    cy.contains('Get Mooney').should('exist')
+    cy.contains('Create Project').should('exist')
     cy.contains('Get Rewards').should('exist')
+    cy.contains('Explore the Network Map').should('exist')
+    cy.contains('Connect to Guild.xyz').should('exist')
+    cy.contains('Create a Team').should('exist')
   })
 
   it('Displays complete profile action if incomplete profile', () => {
-    cy.mount(<CitizenActions {...props} nft={incompleteNft} />)
+    cy.mount(<CitizenActions {...props} incompleteProfile />)
     cy.contains('Complete Profile').should('exist')
+  })
+
+  it('Displays lock action if user has mooney but no vmooney', () => {
+    cy.mount(
+      <CitizenActions {...props} mooneyBalance={100} vmooneyBalance={0} />
+    )
+    cy.contains('Lock to Vote').should('exist')
   })
 })
