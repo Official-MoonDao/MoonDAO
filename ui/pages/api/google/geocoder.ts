728eb1ac79a65111498457deab6be8ca6612b5c2
colinmfoster4723 <colin.foster4723@gmail.com>
2024-11-18 19:53:07 -0600

    Adjusted storing of citizen location to optimize network map

---
+++ ui/pages/api/google/geocoder.ts
@@ -0,0 +1,24 @@
+import { privyAuth } from 'middleware/privyAuth'
+import withMiddleware from 'middleware/withMiddleware'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { location } = req.body
+    console.log('location', location)
+    try {
+      const locationRes = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+      )
+      const locationData = await locationRes.json()
+      return res.send({ data: locationData })
+    } catch (error) {
+      console.error('Error processing request:', error)
+      return res.status(500).json('Error processing request')
+    }
+  } else {
+    return res.status(400).json('Invalid method')
+  }
+}
+
+export default withMiddleware(handler, privyAuth)
