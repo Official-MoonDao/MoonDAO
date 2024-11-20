76d6bd16f0dc956dac11a26bf242dba6264d7924
namedotget <colin.foster4723@gmail.com>
2024-11-19 18:49:15 -0600

    Optimized pretty links

--- ui/pages/citizen/[tokenIdOrName].tsx
+++ ui/pages/citizen/[tokenIdOrName].tsx
@@ -568,6 +568,12 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       tokenId = prettyLinks[tokenIdOrName]
     }
 
+    if (tokenId === undefined) {
+      return {
+        notFound: true,
+      }
+    }
+
     const citizenContract = await sdk.getContract(
       CITIZEN_ADDRESSES[chain.slug],
       CitizenABI
