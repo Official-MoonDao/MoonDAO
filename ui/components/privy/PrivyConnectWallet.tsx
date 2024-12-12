a203c7a4a97b35e8d96f99985ebe08c0f2ce226e
namedotget <colin.foster4723@gmail.com>
2024-12-12 10:50:11 -0600

    Disabled citizen auto-routing when on submission page

--- ui/components/privy/PrivyConnectWallet.tsx
+++ ui/components/privy/PrivyConnectWallet.tsx
@@ -230,7 +230,9 @@ export function PrivyConnectWallet({
   const { login } = useLogin({
     onComplete: async (user, isNewUser, wasAlreadyAuthenticated) => {
       //If the user signs in and wasn't already authenticated, check if they have a citizen NFT and redirect them to their profile or the guest page
-      if (!wasAlreadyAuthenticated) {
+      console.log(router)
+
+      if (!wasAlreadyAuthenticated && router.pathname !== '/submission') {
         let citizen
         try {
           const citizenContract = await sdk?.getContract(
