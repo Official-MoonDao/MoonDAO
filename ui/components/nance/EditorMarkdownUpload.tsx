f7e4f44a4e4e7157658ac1ae101f4a71c930a7cd
namedotget <colin.foster4723@gmail.com>
2024-12-13 12:50:53 -0600

    Removed unused import

--- ui/components/nance/EditorMarkdownUpload.tsx
+++ ui/components/nance/EditorMarkdownUpload.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import toast from 'react-hot-toast'
 import toastStyle from '@/lib/marketplace/marketplace-utils/toastConfig'
 
