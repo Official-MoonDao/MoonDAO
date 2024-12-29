ad2e6ed8987c0e3e91f6522a569cacdbf7173c29
jigglyjams <jigglyjams@protonmail.com>
2024-12-29 12:35:42 -0500

    fix(nance/constants): put correct constants back

--- ui/lib/nance/constants.ts
+++ ui/lib/nance/constants.ts
@@ -1,8 +1,8 @@
 // use chainId as env var for detecting dev mode
 const prod = process.env.NEXT_PUBLIC_CHAIN === 'mainnet'
-export const NANCE_SPACE_NAME = 'waterbox'
+export const NANCE_SPACE_NAME = prod ? 'moondao' : 'waterbox'
 export const SNAPSHOT_SPACE_NAME = prod ? 'tomoondao.eth' : 'jigglyjams.eth'
 
 export const proposalIdPrefix = 'MDP-';
 
-export const NANCE_API_URL = 'http://localhost:3003'
+export const NANCE_API_URL = prod ? 'https://api.nance.app' : 'http://localhost:3003'
