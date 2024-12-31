8c0b0e4070d3a5fdbf6f1e6000b6921f56076055
namedotget <colin.foster4723@gmail.com>
2024-12-30 18:32:51 -0600

    Refactor

--- ui/lib/team/useTeamData.tsx
+++ ui/lib/team/useTeamData.tsx
@@ -1,5 +1,5 @@
 import { useAddress } from '@thirdweb-dev/react'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useHandleRead } from '../thirdweb/hooks'
 import { getAttribute } from '../utils/nft'
 
@@ -7,7 +7,6 @@ export function useTeamData(teamContract: any, hatsContract: any, nft: any) {
   const address = useAddress()
 
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [socials, setSocials] = useState<any>()
   const [isPublic, setIsPublic] = useState<boolean>(false)
   const [isDeleted, setIsDeleted] = useState<boolean>(false)
   const [isManager, setIsManager] = useState<boolean>(false)
@@ -22,49 +21,48 @@ export function useTeamData(teamContract: any, hatsContract: any, nft: any) {
     nft?.metadata?.id || '',
   ])
 
-  function getView() {
-    const entityView: any = getAttribute(nft.metadata.attributes, 'view')
-    setIsPublic(entityView?.value === 'public' ? true : false)
-    setIsDeleted(entityView?.value === '' ? true : false)
-  }
-
-  function getEntitySocials() {
+  const socials = useMemo(() => {
     const entityTwitter = getAttribute(nft.metadata.attributes, 'twitter')
     const entityCommunications = getAttribute(
       nft.metadata.attributes,
       'communications'
     )
     const entityWebsite = getAttribute(nft.metadata.attributes, 'website')
-    setSocials({
+    return {
       twitter: entityTwitter?.value,
       communications: entityCommunications?.value,
       website: entityWebsite?.value,
-    })
-  }
-
-  async function checkSubscription() {
-    //get unix timestamp for now
-    const now = Math.floor(Date.now() / 1000)
-
-    try {
-      const expiresAt = await teamContract.call('expiresAt', [
-        nft?.metadata?.id,
-      ])
-      setSubIsValid(expiresAt.toNumber() > now)
-    } catch (err) {
-      console.log(err)
     }
-  }
+  }, [nft.metadata.attributes])
 
   useEffect(() => {
-    if (!nft?.metadata?.attributes || !teamContract) return
-    ;(async () => {
-      setIsLoading(true)
-      await checkSubscription()
-      getEntitySocials()
-      getView()
-      setIsLoading(false)
-    })()
+    function getView() {
+      const entityView: any = getAttribute(nft.metadata.attributes, 'view')
+      setIsPublic(entityView?.value === 'public' ? true : false)
+      setIsDeleted(entityView?.value === '' ? true : false)
+    }
+
+    async function checkSubscription() {
+      const now = Math.floor(Date.now() / 1000)
+
+      try {
+        const expiresAt = await teamContract.call('expiresAt', [
+          nft?.metadata?.id,
+        ])
+        setSubIsValid(expiresAt.toNumber() > now)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    if (nft?.metadata?.attributes && teamContract) {
+      ;(async () => {
+        setIsLoading(true)
+        await checkSubscription()
+        getView()
+        setIsLoading(false)
+      })()
+    }
   }, [nft, teamContract])
 
   useEffect(() => {
