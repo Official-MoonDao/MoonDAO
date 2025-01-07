570a660e3e0ff5f38202fb873c9654889601876d
namedotget <colin.foster4723@gmail.com>
2025-01-07 10:24:15 -0600

    Adjusted metadata for submit page

--- ui/pages/submit.tsx
+++ ui/pages/submit.tsx
@@ -1,64 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { Tab } from '@headlessui/react';
-import { NanceProvider } from "@nance/nance-hooks";
-import { NANCE_API_URL } from "../lib/nance/constants";
-import Container from '../components/layout/Container';
-import ContentLayout from '../components/layout/ContentLayout';
-import ProposalEditor from '../components/nance/ProposalEditor';
-import ContributionEditor from '../components/contribution/ContributionEditor';
-import WebsiteHead from '../components/layout/Head';
-import { NoticeFooter } from '../components/layout/NoticeFooter';
-import { useRouter } from 'next/router';
+import { Tab } from '@headlessui/react'
+import { NanceProvider } from '@nance/nance-hooks'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { NANCE_API_URL } from '../lib/nance/constants'
+import { useShallowQueryRoute } from '@/lib/utils/hooks'
+import ContributionEditor from '../components/contribution/ContributionEditor'
+import Container from '../components/layout/Container'
+import ContentLayout from '../components/layout/ContentLayout'
+import WebsiteHead from '../components/layout/Head'
+import { NoticeFooter } from '../components/layout/NoticeFooter'
+import ProposalEditor from '../components/nance/ProposalEditor'
 
 const SubmissionPage: React.FC = () => {
-  const router = useRouter();
-  const { tag } = router.query;
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const title = 'Collaborate with MoonDAO';
+  const router = useRouter()
+  const { tag } = router.query
+  const shallowQueryRoute = useShallowQueryRoute()
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const title = 'Collaborate with MoonDAO'
+
+  useEffect(() => {
+    if (selectedIndex === 1) {
+      shallowQueryRoute({ tag: 'contribution' })
+    } else if (selectedIndex === 0) {
+      shallowQueryRoute({ tag: 'proposal' })
+    }
+  }, [selectedIndex])
 
   useEffect(() => {
     if (tag === 'contribution') {
-      setSelectedIndex(1);
-    } else {
-      setSelectedIndex(0);
+      setSelectedIndex(1)
+    } else if (!tag || tag === 'proposal') {
+      setSelectedIndex(0)
     }
-  }, [tag]);
-
-  const headerContent = (
-    <div>
-    </div>
-  );
+  }, [tag])
 
   return (
     <>
-      <WebsiteHead title={title} description={headerContent} />
+      <WebsiteHead title={title} description="" />
       <section className="flex flex-col justify-center items-start animate-fadeIn w-[90vw] md:w-full">
         <Container>
           <ContentLayout
             header="Submissions"
             headerSize="40px"
-            description={headerContent}
+            description={<div></div>}
             mainPadding
             mode="compact"
             isProfile={true}
           >
             <div className="flex flex-col gap-4 p-5 bg-slide-section rounded-tl-[2vw] rounded-bl-[2vw]">
-              <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+              <Tab.Group
+                selectedIndex={selectedIndex}
+                onChange={setSelectedIndex}
+              >
                 <Tab.List className="flex rounded-xl">
-                  <Tab className={({ selected }) =>
-                    `rounded-lg py-2.5 px-5 font-GoodTimes leading-5 focus:outline-none
-                    ${selected
-                      ? 'bg-gradient-to-r from-[#5757ec] to-[#6b3d79] text-white shadow'
-                      : 'text-white/70 hover:text-white'}`
-                  }>
+                  <Tab
+                    className={({ selected }) =>
+                      `rounded-lg py-2.5 px-5 font-GoodTimes leading-5 focus:outline-none
+                    ${
+                      selected
+                        ? 'bg-gradient-to-r from-[#5757ec] to-[#6b3d79] text-white shadow'
+                        : 'text-white/70 hover:text-white'
+                    }`
+                    }
+                  >
                     Submit Proposal
                   </Tab>
-                  <Tab className={({ selected }) =>
-                    `rounded-lg py-2.5 font-GoodTimes leading-5 px-5 focus:outline-none
-                    ${selected
-                      ? 'bg-gradient-to-r from-[#5757ec] to-[#6b3d79] text-white shadow'
-                      : 'text-white/70 hover:text-white'}`
-                  }>
+                  <Tab
+                    className={({ selected }) =>
+                      `rounded-lg py-2.5 font-GoodTimes leading-5 px-5 focus:outline-none
+                    ${
+                      selected
+                        ? 'bg-gradient-to-r from-[#5757ec] to-[#6b3d79] text-white shadow'
+                        : 'text-white/70 hover:text-white'
+                    }`
+                    }
+                  >
                     Submit Contribution
                   </Tab>
                 </Tab.List>
@@ -66,11 +82,15 @@ const SubmissionPage: React.FC = () => {
                   <Tab.Panel>
                     <div className="mb-8">
                       <p className="text-gray-300">
-                        Submit a proposal to receive financing or special permissions from the MoonDAO community. Please refer to {' '}
-                        <a href="https://docs.moondao.com/Projects/Project-System" className="text-blue-400 hover:text-blue-300 underline">
+                        Submit a proposal to receive financing or special
+                        permissions from the MoonDAO community. Please refer to{' '}
+                        <a
+                          href="https://docs.moondao.com/Projects/Project-System"
+                          className="text-blue-400 hover:text-blue-300 underline"
+                        >
                           our documentation
-                        </a>
-                        {' '}for more details before getting started.
+                        </a>{' '}
+                        for more details before getting started.
                       </p>
                     </div>
                     <NanceProvider apiUrl={NANCE_API_URL}>
@@ -80,11 +100,16 @@ const SubmissionPage: React.FC = () => {
                   <Tab.Panel>
                     <div className="mb-8">
                       <p className="text-gray-300">
-                        What have you done to accelerate the impact of MoonDAO's mission? Submit your results for senate review and potential rewards. Please refer to {' '}
-                        <a href="https://docs.moondao.com/Projects/Project-System" className="text-blue-400 hover:text-blue-300 underline">
+                        What have you done to accelerate the impact of MoonDAO's
+                        mission? Submit your results for senate review and
+                        potential rewards. Please refer to{' '}
+                        <a
+                          href="https://docs.moondao.com/Projects/Project-System"
+                          className="text-blue-400 hover:text-blue-300 underline"
+                        >
                           our documentation
-                        </a>
-                        {' '}for more details.
+                        </a>{' '}
+                        for more details.
                       </p>
                     </div>
                     <ContributionEditor />
@@ -97,7 +122,7 @@ const SubmissionPage: React.FC = () => {
         </Container>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default SubmissionPage;
+export default SubmissionPage
