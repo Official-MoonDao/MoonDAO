57906978b30d46fe0177baf1b2b998598c5d3cc8
namedotget <colin.foster4723@gmail.com>
2025-01-07 13:45:16 -0600

    Fixed input unfocus bug

--- ui/components/layout/NumberStepper.tsx
+++ ui/components/layout/NumberStepper.tsx
@@ -1,4 +1,5 @@
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline'
+import { useRef } from 'react'
 
 type NumberStepperProps = {
   number: number
@@ -15,11 +16,14 @@ export default function NumberStepper({
   max,
   min,
 }: NumberStepperProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
   function increase() {
     if (max && number + step > max) {
       return
     }
     setNumber(number + step)
+    inputRef.current?.focus()
   }
 
   function decrease() {
@@ -27,15 +31,19 @@ export default function NumberStepper({
       return
     }
     setNumber(number - step)
+    inputRef.current?.focus()
   }
 
   return (
     <div className="flex items-center justify-between w-[100px] min-h-[35px] gradient-2 rounded-full">
       <input
+        ref={inputRef}
+        id="number-stepper"
         className="w-[55%] h-full bg-[#00000080] text-white text-center rounded-full [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
         type="number"
         value={number}
         onChange={(e) => setNumber(Number(e.target.value))}
+        step={0}
       />
       <div className="w-[45%] flex flex-col justify-center items-center">
         <button onClick={increase}>
