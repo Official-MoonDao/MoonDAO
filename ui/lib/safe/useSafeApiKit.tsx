8848208f809e140008cf89f856a8cdf4d337bd3d
namedotget <colin.foster4723@gmail.com>
2025-01-14 20:25:43 -0600

    Upgraded safe

--- ui/lib/safe/useSafeApiKit.tsx
+++ ui/lib/safe/useSafeApiKit.tsx
@@ -1,35 +1,40 @@
+import { useWallets } from '@privy-io/react-auth'
 import SafeApiKit from '@safe-global/api-kit'
-import { EthersAdapter } from '@safe-global/protocol-kit'
-import { useSDK } from '@thirdweb-dev/react'
-import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
+import Safe from '@safe-global/protocol-kit'
+import { useContext, useEffect, useState } from 'react'
+import PrivyWalletContext from '../privy/privy-wallet-context'
+import ChainContextV5 from '../thirdweb/chain-context-v5'
 
-export default function useSafeApiKit() {
-  const sdk = useSDK()
+export default function useSafeApiKit(safeAddress: string) {
+  const { selectedWallet } = useContext(PrivyWalletContext)
+  const { selectedChain } = useContext(ChainContextV5)
+  const { wallets } = useWallets()
 
   const [safeApiKit, setSafeApiKit] = useState<any>()
+  const [protocolKit, setProtocolKit] = useState<any>()
 
   useEffect(() => {
-    const signer = sdk?.getSigner()
-    if (!sdk || !signer) return
+    async function getSafeApiKit() {
+      const apiKit = new SafeApiKit({
+        chainId: BigInt(selectedChain.id),
+      })
+      setSafeApiKit(apiKit)
+    }
 
-    const ethAdapter = new EthersAdapter({
-      ethers,
-      signerOrProvider: signer,
-    })
+    async function getProtocolKit() {
+      const privyProvider = await wallets[selectedWallet]?.getEthereumProvider()
 
-    const safeNetwork =
-      process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? 'arbitrum' : 'sepolia'
+      if (!privyProvider) return
 
-    const txServiceUrl = `https://safe-transaction-${safeNetwork}.safe.global`
+      const protoKit = await Safe.init({
+        provider: privyProvider as any,
+        safeAddress,
+      })
+      setProtocolKit(protoKit)
+    }
+    getSafeApiKit()
+    getProtocolKit()
+  }, [selectedWallet, wallets, selectedChain, safeAddress])
 
-    const apiKit = new SafeApiKit({
-      txServiceUrl,
-      ethAdapter,
-    })
-
-    setSafeApiKit(apiKit)
-  }, [sdk])
-
-  return safeApiKit
+  return { safeApiKit, protocolKit }
 }
