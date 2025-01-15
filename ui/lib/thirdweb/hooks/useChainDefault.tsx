d9bb7807b60dcb88956748f58c3077e761782436
namedotget <colin.foster4723@gmail.com>
2025-01-14 20:31:42 -0600

    Configured providers and hooks for thirdweb v5

--- ui/lib/thirdweb/hooks/useChainDefault.tsx
+++ ui/lib/thirdweb/hooks/useChainDefault.tsx
@@ -1,16 +1,19 @@
-import {
-  Arbitrum,
-  Sepolia,
-} from '@thirdweb-dev/chains'
-import { useContext, useEffect, useMemo } from 'react'
+import { Arbitrum, Sepolia } from '@thirdweb-dev/chains'
+import { useContext, useEffect } from 'react'
+import { arbitrum, sepolia } from 'thirdweb/chains'
 import ChainContext from '../chain-context'
+import ChainContextV5 from '../chain-context-v5'
 
 export function useChainDefault() {
   const { setSelectedChain } = useContext(ChainContext)
+  const { setSelectedChain: setSelectedChainV5 } = useContext(ChainContextV5)
 
   useEffect(() => {
     setSelectedChain(
       process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? Arbitrum : Sepolia
     )
-  }, [setSelectedChain])
+    setSelectedChainV5(
+      process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ? arbitrum : sepolia
+    )
+  }, [setSelectedChain, setSelectedChainV5])
 }
