82df7a8c54a07a99257bcac95861ea3d47537e16
namedotget <colin.foster4723@gmail.com>
2025-01-27 18:46:35 -0600

    Added create proposal button

--- ui/pages/vote.tsx
+++ ui/pages/vote.tsx
@@ -1,5 +1,6 @@
 import {
   PlusIcon,
+  PlusCircleIcon,
   QueueListIcon,
   MagnifyingGlassIcon,
 } from '@heroicons/react/24/solid'
@@ -16,6 +17,7 @@ import ContentLayout from '@/components/layout/ContentLayout'
 import Frame from '@/components/layout/Frame'
 import { NoticeFooter } from '@/components/layout/NoticeFooter'
 import Search from '@/components/layout/Search'
+import StandardButton from '@/components/layout/StandardButton'
 
 const links = [
   {
@@ -93,6 +95,14 @@ export default function SpaceIndex() {
         </u>{' '}
         for more details on the project system and governance processes.
       </p>
+      <Link href="/submit" passHref>
+        <StandardButton className="mt-4 gradient-2 rounded-full">
+          <div className="flex items-center gap-2">
+            <PlusCircleIcon className="w-6 h-6" />
+            Create a Proposal
+          </div>
+        </StandardButton>
+      </Link>
     </div>
   )
 
