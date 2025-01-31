b82636cf9fcd6d82dbc5abf6d37ce6bf38439575
namedotget <colin.foster4723@gmail.com>
2025-01-31 15:17:27 -0600

    Hide /join nav link if citizen

--- ui/lib/navigation/useNavigation.tsx
+++ ui/lib/navigation/useNavigation.tsx
@@ -13,14 +13,10 @@ import IconOrg from '@/components/assets/IconOrg'
 export default function useNavigation(citizen: any) {
   const navigation = useMemo(() => {
     return [
-      {
+      !citizen && {
         name: 'Join',
         href: '/join',
         icon: PlusIcon,
-        children: [
-          !citizen && { name: 'Become a Citizen', href: '/citizen' },
-          { name: 'Create a Team', href: '/team' },
-        ],
       },
       {
         name: 'Network',
