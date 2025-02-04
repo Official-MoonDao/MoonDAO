762441f1d2aafde9824c21ca6309f8d38159177a
name.get <99998750+namedotget@users.noreply.github.com>
2025-02-04 00:29:19 -0600

    Fix marketplace api route and send email to buyer (#362)
    * Updated marketplace and jobboard addresses for sepolia
    
    * Convert bigint to number
    
    * Updated marketplace purchase to send email to buyer, removed blocknumber check
    
    * bcc opEmail for buyer email

--- ui/pages/api/marketplace/marketplace-purchase.ts
+++ ui/pages/api/marketplace/marketplace-purchase.ts
@@ -8,17 +8,28 @@ import { transporter, opEmail } from '@/lib/nodemailer/nodemailer'
 import { getChainSlug } from '@/lib/thirdweb/chain'
 import { serverClient } from '@/lib/thirdweb/client'
 
-const MARKETPLACE_PURHCASE_FIELDS: any = {
+const MARKETPLACE_VENDOR_PURHCASE_FIELDS: any = {
   address: 'Address',
   email: 'Email',
   item: 'Item',
   tx: 'Transaction',
 }
 
-const generateEmailContent = (data: any) => {
+const MARKETPLACE_CITIZEN_PURHCASE_FIELDS: any = {
+  item: 'Item',
+  value: 'Value',
+  quantity: 'Quantity',
+  tx: 'Transaction',
+}
+
+const generateHTML = (htmlData: any) => {
+  return `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>MoonDAO Marketplace Purchase</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`
+}
+
+const generateVendorEmailContent = (data: any) => {
   const stringData = Object.entries(data).reduce(
     (str, [key, val]) =>
-      (str += `${MARKETPLACE_PURHCASE_FIELDS[key]}: \n${val} \n \n`),
+      (str += `${MARKETPLACE_VENDOR_PURHCASE_FIELDS[key]}: \n${val} \n \n`),
     ''
   )
 
@@ -62,7 +73,37 @@ const generateEmailContent = (data: any) => {
 
   return {
     text: stringData,
-    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>MoonDAO Marketplace Purchase</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+    html: generateHTML(htmlData),
+  }
+}
+
+const generateCitizenEmailContent = (data: any) => {
+  const stringData = Object.entries(data).reduce(
+    (str, [key, val]) =>
+      (str += `${MARKETPLACE_CITIZEN_PURHCASE_FIELDS[key]}: \n${val} \n \n`),
+    ''
+  )
+
+  const { item, value, currency, quantity, txLink, teamLink } = JSON.parse(data)
+
+  const htmlData = `
+  <div>
+    <label for="teamLink"><strong>Team</strong></label>
+    <p>${teamLink}</p>
+    <label for="item"><strong>Item</strong></label>
+    <p>${item}</p>
+    <label for="value"><strong>Value</strong></label>
+    <p>${value} ${currency}</p>
+    <label for="quantity"><strong>Quantity</strong></label>
+    <p>${quantity}</p>
+    <label for="tx"><strong>Transaction</strong></label>
+    <p>${txLink}</p>
+  </div>
+  `
+
+  return {
+    text: stringData,
+    html: generateHTML(htmlData),
   }
 }
 
@@ -75,6 +116,7 @@ async function handler(req: any, res: any) {
 
     const {
       teamEmail,
+      email,
       txHash,
       isCitizen,
       recipient,
@@ -120,7 +162,10 @@ async function handler(req: any, res: any) {
     }
 
     if (fromIsNotCitizen) {
-      return res.status(400).json({ message: 'Citizen cannot be verified' })
+      return res.status(400).json({
+        message:
+          'The transaction went through but the citizen cannot be verified',
+      })
     }
 
     const provider = ethers5Adapter.provider.toEthers({
@@ -129,29 +174,39 @@ async function handler(req: any, res: any) {
     })
     const currBlockNumber = await provider?.getBlockNumber()
 
-    if (currBlockNumber - +txReceipt.blockNumber.toString() > 2) {
-      return res.status(400).json({ message: 'Transaction is invalid' })
-    }
-
     if (recipient.toLowerCase() !== txReceipt.to?.toLowerCase()) {
-      return res.status(400).json({ message: 'Recipient is incorrect' })
+      return res.status(400).json({
+        message: 'The transaction went through but the recipient is incorrect',
+      })
     }
 
     const txValue = Number(txReceipt.logs[0]?.data) / 10 ** +decimals
     if (+value !== txValue) {
-      return res.status(400).json({ message: 'Transaction is invalid' })
+      return res.status(400).json({
+        message: 'The transaction went through but the value is incorrect',
+      })
     }
 
     if (!verifiedCitizen && txValue !== originalValue + originalValue * 0.1) {
-      return res.status(400).json({ message: 'Transaction is invalid' })
+      return res.status(400).json({
+        message: 'The transaction went through but the value is incorrect',
+      })
     }
 
     try {
       await transporter.sendMail({
         from: opEmail,
         to: teamEmail,
-        ...generateEmailContent(data),
+        ...generateVendorEmailContent(data),
         subject: 'MoonDAO | Marketplace Purchase',
+      })
+
+      await transporter.sendMail({
+        from: opEmail,
+        to: email,
+        ...generateCitizenEmailContent(data),
+        subject: 'MoonDAO | Marketplace Purchase',
+        bcc: [opEmail],
       })
 
       return res.status(200).json({ success: true })
