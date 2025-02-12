6990a6d7befb10fece7139d8f3b84167ac22f7db
namedotget <colin.foster4723@gmail.com>
2025-02-12 10:18:21 -0600

    Updated tests

---
+++ ui/cypress/integration/layout/standard-card.cy.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import StandardCard from '../../../components/layout/StandardCard'
+
+describe('<StandardCard />', () => {
+  it('renders with basic props', () => {
+    cy.mountNextRouter('/')
+    cy.mount(
+      <StandardCard
+        icon="/assets/test-icon.svg"
+        header="Test Header"
+        paragraph="Test paragraph"
+        hovertext="Test hovertext"
+      />
+    )
+
+    cy.get('#card-container').should('exist')
+    cy.get('#featured-icon').should('be.visible')
+    cy.get('#main-header').should('contain', 'Test Header')
+  })
+
+  it('renders organization image when provided', () => {
+    cy.mountNextRouter('/')
+    cy.mount(<StandardCard orgimage="/test-org-image.jpg" header="Org Card" />)
+
+    cy.get('#featured-image').should('be.visible')
+    cy.get('#featured-image')
+      .should('have.attr', 'src')
+      .and('include', 'url=%2Ftest-org-image.jpg')
+  })
+
+  it('renders header link when provided', () => {
+    cy.mountNextRouter('/')
+    cy.mount(
+      <StandardCard
+        header="Link Test"
+        headerLink="/test-link"
+        headerLinkLabel="Click Here"
+      />
+    )
+
+    cy.get('button').contains('Click Here').should('exist')
+  })
+
+  it('shows hovertext on desktop', () => {
+    cy.mountNextRouter('/')
+    cy.viewport('macbook-15')
+    cy.mount(<StandardCard header="Hover Test" hovertext="Test hovertext" />)
+
+    cy.get('#hovertext')
+      .should('have.class', 'hidden')
+      .and('have.class', 'md:block')
+    cy.get('#card-container').trigger('mouseover')
+    cy.get('#hovertext').should('be.visible').and('contain', 'Test hovertext')
+  })
+
+  it('shows mobile button instead of hovertext on mobile', () => {
+    cy.mountNextRouter('/')
+    cy.viewport('iphone-x')
+    cy.mount(<StandardCard header="Mobile Test" hovertext="Test hovertext" />)
+
+    cy.get('#mobile-button-container').should('be.visible')
+    cy.get('#hovertext').should('have.class', 'hidden')
+  })
+
+  it('renders link when provided', () => {
+    cy.mountNextRouter('/')
+    cy.mount(<StandardCard header="Link Test" link="/test-link" />)
+
+    cy.get('#card-link').should('have.attr', 'href', '/test-link')
+  })
+
+  it('handles onClick when provided', () => {
+    const onClickSpy = cy.spy().as('onClickSpy')
+    cy.mountNextRouter('/')
+    cy.mount(<StandardCard header="Click Test" onClick={onClickSpy} />)
+
+    cy.get('button').click()
+    cy.get('@onClickSpy').should('have.been.called')
+  })
+})
