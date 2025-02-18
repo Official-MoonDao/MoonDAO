0679e14a932e1b2bfb8ac8081f479cad9a3a6c6b
namedotget <colin.foster4723@gmail.com>
2025-02-18 14:50:07 -0600

    Fix links

--- ui/lib/navigation/useNavigation.tsx
+++ ui/lib/navigation/useNavigation.tsx
@@ -23,8 +23,8 @@ export default function useNavigation(citizen: any) {
         href: '/network',
         icon: IconOrg,
         children: [
-          { name: 'Citizens', href: '/citizens' },
-          { name: 'Teams', href: '/teams' },
+          { name: 'Citizens', href: '/network?tab=citizens' },
+          { name: 'Teams', href: '/network?tab=teams' },
           { name: 'Map', href: '/map' },
           { name: 'Create a Team', href: '/team' },
         ],
