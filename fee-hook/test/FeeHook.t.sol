befdf780808034f952834f7e77d102a5a6fb12ec
jaderiverstokes <jaderiverstokes@gmail.com>
2025-04-15 13:31:32 -0700

    adds tests to ci

---
+++ fee-hook/test/FeeHook.t.sol
@@ -0,0 +1,196 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {FakeERC20} from "../src/FakeERC20.sol";
+import {Actions} from "v4-periphery/src/libraries/Actions.sol";
+import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
+import {PoolId} from "v4-core/src/types/PoolId.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {Constants} from "v4-core/src/../test/utils/Constants.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import { UniversalRouter } from "@uniswap/universal-router/contracts/UniversalRouter.sol";
+import { IV4Router } from "v4-periphery/src/interfaces/IV4Router.sol";
+import {FeeHook} from "../src/FeeHook.sol";
+import {HookMiner} from "v4-periphery/src/utils/HookMiner.sol";
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IWETH9} from "v4-periphery/src/interfaces/external/IWETH9.sol";
+
+contract FeeHookTest is Test {
+    using StateLibrary for IPoolManager;
+    uint24 constant FEE = 10000;
+    uint256 constant V4_SWAP = 0x10;
+
+    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
+    IPoolManager manager;
+    address poolManagerAddress;
+    IPositionManager posm;
+    UniversalRouter router;
+    IAllowanceTransfer permit2;
+    address posmAddress;
+    address lzEndpoint = 0x1a44076050125825900e736c501f859c50fE728c;
+    uint256 DESTINATION_CHAIN_ID = 1;
+    uint16 DESTINATION_EID = 30101;
+    address fakeTokenAddress;
+    FeeHook feeHook;
+
+    address user1 = address(0x1);
+    address deployerAddress = address(0x2);
+
+    function setUp() public {
+        // v4 mainnet addresses
+        poolManagerAddress = 0x000000000004444c5dc75cB358380D2e3dE08A90;
+        manager = IPoolManager(poolManagerAddress);
+        address permit2Address = 0x000000000022D473030F116dDEE9F6B43aC78BA3;
+        permit2 = IAllowanceTransfer(permit2Address);
+        posmAddress = 0xbD216513d74C8cf14cf4747E6AaA6420FF64ee9e;
+        posm = IPositionManager(posmAddress);
+        address payable routerAddress = payable(0x66a9893cC07D91D95644AEDD05D03f95e1dBA8Af);
+        router = UniversalRouter(routerAddress);
+        vm.deal(address(manager), 100_000_000 ether);
+        vm.deal(address(posm), 100_000_000 ether);
+        vm.deal(deployerAddress, 100_000_000_000 ether);
+
+        vm.startBroadcast(deployerAddress);
+        FakeERC20 fakeToken = new FakeERC20(100_000, "Fake Token", "FAKE");
+        fakeTokenAddress = address(fakeToken);
+        vm.stopBroadcast();
+    }
+
+    function testFeeHook() public {
+        vm.startBroadcast(deployerAddress);
+        feeHook = deployHook();
+        vm.stopBroadcast();
+        address hookAddress = address(feeHook);
+
+        // test the lifecycle (create pool, add liquidity, swap)
+        vm.startBroadcast(deployerAddress);
+        testLifecycle(hookAddress);
+        vm.stopBroadcast();
+    }
+
+    function deployHook() internal returns (FeeHook) {
+        // hook contracts must have specific flags encoded in the address
+        uint160 permissions = uint160(
+            Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
+        );
+
+        // Mine a salt that will produce a hook address with the correct permissions
+        (address hookAddress, bytes32 salt) =
+            HookMiner.find(CREATE2_DEPLOYER, permissions, type(FeeHook).creationCode, abi.encode(deployerAddress, poolManagerAddress, posmAddress, lzEndpoint, DESTINATION_CHAIN_ID, DESTINATION_EID, fakeTokenAddress));
+
+        feeHook = new FeeHook{salt: salt}(deployerAddress, IPoolManager(poolManagerAddress), IPositionManager(posmAddress), lzEndpoint, DESTINATION_CHAIN_ID, DESTINATION_EID, fakeTokenAddress);
+        require(address(feeHook) == hookAddress, "FeeHookTest: hook address mismatch");
+        return feeHook;
+    }
+
+    function deployToken(address recipient) internal returns (MockERC20 token1) {
+        token1 = new MockERC20("Mock", "MOCK", 18);
+        token1.mint(recipient, 100_000 ether);
+        Currency currency = Currency.wrap(address(token1));
+        // Because POSM uses permit2, we must execute 2 permits/approvals.
+        // 1. First, the caller must approve permit2 on the token.
+        IERC20(Currency.unwrap(currency)).approve(address(permit2), type(uint256).max);
+        // 2. Then, the caller must approve POSM as a spender of permit2
+        permit2.approve(Currency.unwrap(currency), address(posm), type(uint160).max, type(uint48).max);
+    }
+
+    function testLifecycle(address hookAddress) internal {
+        MockERC20 token1 = deployToken(deployerAddress);
+
+        // initialize the pool
+        int24 tickSpacing = 60;
+        PoolKey memory poolKey =
+            PoolKey(CurrencyLibrary.ADDRESS_ZERO, Currency.wrap(address(token1)), FEE, tickSpacing, IHooks(hookAddress));
+        manager.initialize(poolKey, Constants.SQRT_PRICE_1_1);
+
+        // add full range liquidity to the pool
+        int24 tickLower = TickMath.minUsableTick(tickSpacing);
+        int24 tickUpper = TickMath.maxUsableTick(tickSpacing);
+        mintLiquidity(poolKey, tickLower, tickUpper, hookAddress);
+
+        // swap some tokens
+        swap(poolKey);
+
+        uint256 balanceBefore = address(deployerAddress).balance;
+        feeHook.withdrawFees();
+        uint256 balanceAfter = address(deployerAddress).balance;
+
+        feeHook.transferPosition(
+            deployerAddress,
+            PoolId.unwrap(poolKey.toId())
+        );
+        uint256 tokenId = feeHook.TOKEN_ID(poolKey.toId());
+
+        // burn the position
+        burn(poolKey, tokenId, deployerAddress);
+        uint256 balanceAfter2 = address(deployerAddress).balance;
+
+
+    }
+
+    function burn(PoolKey memory poolKey, uint256 tokenId, address recipient) internal {
+        bytes memory actions = abi.encodePacked(uint8(Actions.BURN_POSITION), uint8(Actions.TAKE_PAIR));
+        bytes[] memory params = new bytes[](2);
+        params[0] = abi.encode(tokenId, 0, 0, "");
+        params[1] = abi.encode(poolKey.currency0, poolKey.currency1, recipient);
+        posm.modifyLiquidities(
+            abi.encode(actions, params),
+            block.timestamp + 60
+        );
+    }
+
+    function mintLiquidity(PoolKey memory poolKey, int24 tickLower, int24 tickUpper, address hookAddress) internal returns (uint256){
+        IPoolManager.ModifyLiquidityParams memory liqParams =
+            IPoolManager.ModifyLiquidityParams(tickLower, tickUpper, 100 ether, 0);
+
+        bytes memory actions = abi.encodePacked(uint8(Actions.MINT_POSITION), uint8(Actions.SETTLE_PAIR));
+        bytes[] memory params = new bytes[](2);
+        uint256 valueToPass = 100 ether;
+        params[0] = abi.encode(poolKey, tickLower, tickUpper, valueToPass , 10_000e18, 10_000e18, hookAddress, bytes32(""));
+        params[1] = abi.encode(poolKey.currency0, poolKey.currency1);
+        posm.modifyLiquidities{value: valueToPass}(
+            abi.encode(actions, params),
+            block.timestamp + 300
+        );
+
+        return 0;
+    }
+
+    // trade eth for tokens
+    function swap(PoolKey memory poolKey) internal {
+       uint128 amountSpecified = 1 ether;
+       bytes memory commands = abi.encodePacked(uint8(V4_SWAP));
+       bytes memory actions = abi.encodePacked(
+          uint8(Actions.SWAP_EXACT_IN_SINGLE),
+          uint8(Actions.SETTLE_ALL),
+          uint8(Actions.TAKE_ALL)
+       );
+       bytes[] memory params = new bytes[](3);
+       params[0] = abi.encode(
+           IV4Router.ExactInputSingleParams({
+               poolKey: poolKey,
+               zeroForOne: true,
+               amountIn: amountSpecified,
+               amountOutMinimum: 0,
+               hookData: bytes("")
+           })
+       );
+       params[1] = abi.encode(poolKey.currency0, amountSpecified);
+       params[2] = abi.encode(poolKey.currency1, 0);
+       bytes[] memory inputs = new bytes[](1);
+
+       inputs[0] = abi.encode(actions, params);
+
+       uint256 deadline = block.timestamp + 20;
+       router.execute{value: amountSpecified}(commands, inputs, deadline);
+    }
+}
