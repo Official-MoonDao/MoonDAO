6243c23a7a8b4f252a525f0da63c6a0214bbc90d
namedotget <colin.foster4723@gmail.com>
2025-04-18 16:30:06 -0500

    Removed stages for missions

--- ui/components/mission/MissionTokenExchangeRates.tsx
+++ ui/components/mission/MissionTokenExchangeRates.tsx
@@ -1,36 +1,20 @@
-import {
-  MISSION_STAGE_NAMES,
-  MISSION_TOKEN_WEIGHTS,
-} from '@/lib/mission/missionConfig'
-
 function Rate({
   title,
   rate,
-  stage,
-  currentStage,
   tokenSymbol,
+  active,
 }: {
   title: string
   rate: number
-  stage: number
-  currentStage: number
   tokenSymbol: string
+  active?: boolean
 }) {
   return (
     <div
       className={`${
-        stage === currentStage
-          ? 'bg-darkest-cool'
-          : 'bg-darkest-cool/50 opacity-30'
-      } flex items-center gap-2 rounded-full p-2`}
+        active ? 'bg-darkest-cool' : 'bg-darkest-cool/50 opacity-30'
+      } flex items-center gap-2 rounded-full py-2`}
     >
-      <div
-        className={`w-8 h-8 rounded-full ${
-          stage === currentStage
-            ? 'bg-moon-green'
-            : 'bg-darkest-cool/50 border-grey-400 border-2'
-        }`}
-      />
       <div>
         <h3 className="opacity-60">{title}</h3>
         <p>
@@ -47,37 +31,18 @@ function Rate({
 }
 
 export default function MissionTokenExchangeRates({
-  currentStage,
   tokenSymbol,
 }: {
   currentStage: number
   tokenSymbol: string
 }) {
-  // Default to stage 1 if currentStage is undefined
-  const stage = currentStage || 1
-
   return (
     <div id="mission-token-exchange-rates">
       <Rate
-        title={`Stage 1: ${MISSION_STAGE_NAMES?.[1] ?? ''}`}
-        rate={MISSION_TOKEN_WEIGHTS[0]}
-        stage={1}
-        currentStage={stage}
+        title={`Exchange Rate`}
+        rate={1000}
         tokenSymbol={tokenSymbol}
-      />
-      <Rate
-        title={`Stage 2: ${MISSION_STAGE_NAMES?.[2] ?? ''}`}
-        rate={MISSION_TOKEN_WEIGHTS[1]}
-        stage={2}
-        currentStage={stage}
-        tokenSymbol={tokenSymbol}
-      />
-      <Rate
-        title={`Stage 3: ${MISSION_STAGE_NAMES?.[3] ?? ''}`}
-        rate={MISSION_TOKEN_WEIGHTS[2]}
-        stage={3}
-        currentStage={stage}
-        tokenSymbol={tokenSymbol}
+        active
       />
     </div>
   )
--- ui/components/mission/MissionTokenInfo.tsx
+++ ui/components/mission/MissionTokenInfo.tsx
@@ -44,47 +44,13 @@ export default function MissionTokenInfo({
   fundingGoal: number
 }) {
   //TODO : Add real stage weights and durations
-  const stage1Weight = ruleset?.[0].weight.toString() / 1e18
-  const stage2Weight = stage1Weight / 2
-  const stage3Weight = stage1Weight / 4
-
-  const stage1Duration = ruleset?.[0].duration
-  const stage2Duration = stage1Duration * 2
-  const stage3Duration = stage1Duration * 4
+  const weight = ruleset?.[0].weight.toString() / 1e18
 
   return (
     <div className="flex flex-col gap-4">
       {/* Funding Dynamics / Stages */}
       <h1 className="mt-4 text-2xl font-bold">Funding Dynamics</h1>
       <p>The token funding for this project works in stages.</p>
-      <MissionFundingMilestoneChart
-        fundingGoal={fundingGoal}
-        subgraphData={subgraphData}
-      />
-      {/* <Image/> */}
-      <FundingStage
-        stage={1}
-        title={MISSION_STAGE_NAMES?.[1] ?? ''}
-        description={`During the ignition stage, you get ${MISSION_TOKEN_WEIGHTS[0].toLocaleString()} ${
-          token?.tokenSymbol
-        } per ETH contributed. You can redeem your full amount if the project doesn't raise the required minimum funds within the timeframe.`}
-      />
-      <FundingStage
-        stage={2}
-        title={MISSION_STAGE_NAMES?.[2] ?? ''}
-        description={`If the mission reaches its minimum funding threshold, the project has launched and is ascending toward its goal. At this point you'll receive ${MISSION_TOKEN_WEIGHTS[1].toLocaleString()} ${
-          token?.tokenSymbol
-        } per ETH contributed up to the total goal amount, or until ${new Date(
-          Date.now() + stage2Duration
-        ).toLocaleDateString()}.`}
-      />
-      <FundingStage
-        stage={3}
-        title={MISSION_STAGE_NAMES?.[3] ?? ''}
-        description={`Once the goal amount is reached, you can still continue to contribute until the deadline is reached, but at the rate of ${MISSION_TOKEN_WEIGHTS[2].toLocaleString()} ${
-          token?.tokenSymbol
-        } per ETH contributed.`}
-      />
       <div className="flex items-center gap-2">
         <InformationCircleIcon className="w-16 h-16" />
         <p className="text-sm">{`Note: If no deadline is set, then the project owner can manually trigger the shut down of the project, going into effect 24 hours after the trigger is completed.`}</p>
