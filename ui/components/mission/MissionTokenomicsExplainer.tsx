3ce9576698e540e42c86aca1b6c47f9ea4e4e5a7
ryand2d <rshauers@gmail.com>
2025-04-30 23:40:47 +0200

    remove outdated tokenomics

--- ui/components/mission/MissionTokenomicsExplainer.tsx
+++ ui/components/mission/MissionTokenomicsExplainer.tsx
@@ -5,7 +5,7 @@ export default function MissionTokenomicsExplainer() {
   return (
     <div>
       <Point
-        point="Goal & Timeline: Missions have 28 days, one lunar cycle, to raise at least 20% of the Funding Goal; otherwise, all contributions are refunded to supporters.
+        point="Goal & Timeline: Missions have 28 days, one lunar cycle, to raise their Funding Goal; otherwise, the Mission will not proceed and all contributions can be refunded. Contributors can also withdraw support and get refunded at any time prior to reaching the funding goal.
 "
       />
       <Point
@@ -17,15 +17,6 @@ export default function MissionTokenomicsExplainer() {
       <SubPoint point="10% to liquidity to ensure tradability and market stability." />
       <SubPoint point="7.5% to MoonDAO to support the broader space acceleration ecosystem." />
       <SubPoint point="2.5% to Juicebox, the underlying protocol powering the fundraising infrastructure." />
-
-      <Point point="Mission Tokens: Each mission generates a unique token with a tiered issuance system to incentivize early supporters:" />
-      <SubPoint point="2,000 tokens per 1 ETH until reaching the Minimum (20% of the total goal)." />
-      <SubPoint point="1,000 tokens per 1 ETH until reaching the Funding Goal." />
-      <SubPoint point="500 tokens per 1 ETH for contributions beyond the goal." />
-      <SubPoint
-        point="ERC-20 tokens are not created by default but can be deployed at launch or later, allowing teams flexibility in how they structure governance and incentives.
-"
-      />
 
       <Point point="Mission Ownership & Security: The mission and all associated funds are fully controlled by your team wallet. If teammates fail to maintain access to the wallets connected to the team multisig, funds may become permanently inaccessible, with no recourse for recovery." />
     </div>
