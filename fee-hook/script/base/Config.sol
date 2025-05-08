e7903bfd4950cf040be65d4ea68bb4c3012d067c
jaderiverstokes <jaderiverstokes@gmail.com>
2025-05-08 13:32:23 -0700

    fix build

--- fee-hook/script/base/Config.sol
+++ fee-hook/script/base/Config.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../../lib/forge-std/src/Script.sol";
+import "forge-std/Script.sol";
 
 /// @notice Shared configuration between scripts
 contract Config is Script {
