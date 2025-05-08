dcef830d2566b961289398327bd393a36e6b0444
jaderiverstokes <jaderiverstokes@gmail.com>
2025-05-08 12:59:50 -0700

    fix build

--- subscription-contracts/script/MissionSepolia.s.sol
+++ subscription-contracts/script/MissionSepolia.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import "../src/MissionCreator.sol";
 import "../src/tables/MissionTable.sol";
-import "../../subscription-contracts/script/base/Config.sol";
+import "base/Config.sol";
 
 contract MyScript is Script, Config {
     function run() external {
