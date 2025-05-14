a69b12266af7fe67892c9c28ad9919a7b9eb3ee2
name.get <99998750+namedotget@users.noreply.github.com>
2025-05-14 13:54:08 -0500

    Made /about and /news fullscreen and added tests (#451)

--- ui/components/layout/Layout.tsx
+++ ui/components/layout/Layout.tsx
@@ -49,7 +49,10 @@ export default function Layout({ children, lightMode, setLightMode }: Layout) {
   const { t } = useTranslation('common')
   //Background is defined in this root div.
 
-  const isFullscreen = router.pathname === '/launch'
+  const isFullscreen =
+    router.pathname === '/launch' ||
+    router.pathname === '/about' ||
+    router.pathname === '/news'
 
   const layout = (
     <div
