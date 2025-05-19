2abd626b57add686b354d79b758e3a6502a4ded1
namedotget <colin.foster4723@gmail.com>
2025-05-19 12:02:17 -0400

    Pay component sticky scroll

--- ui/components/mission/MissionInfo.tsx
+++ ui/components/mission/MissionInfo.tsx
@@ -3,7 +3,6 @@ import {
   GlobeAltIcon,
   InformationCircleIcon,
 } from '@heroicons/react/24/outline'
-import gsap from 'gsap'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
@@ -75,12 +74,72 @@ export default function MissionInfo({
 }: any) {
   const router = useRouter()
   const shallowQueryRoute = useShallowQueryRoute()
+  const stickyRef = useRef<HTMLDivElement>(null)
 
-  const payRedeemRef = useRef<HTMLDivElement>(null)
-  const containerRef = useRef<HTMLDivElement>(null)
   const [tab, setTab] = useState<MissionInfoTabType>(
     (router.query.tab as MissionInfoTabType) || 'about'
   )
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const stickyElement = stickyRef.current
+      if (!stickyElement) return
+
+      const parentElement = document.getElementById('mission-info-content')
+      if (!parentElement) return
+
+      const parentRect = parentElement.getBoundingClientRect()
+      const stickyRect = stickyElement.getBoundingClientRect()
+
+      const stickyTop = stickyRect.top
+
+      const stickyPoint = 20
+
+      const parentBottom = parentRect.bottom
+
+      const parentTop = parentRect.top
+
+      const stickyHeight = stickyRect.height
+
+      if (parentTop > stickyPoint) {
+        stickyElement.style.position = 'relative'
+        stickyElement.style.top = '0'
+        stickyElement.style.width = '100%'
+      } else if (
+        stickyTop <= stickyPoint &&
+        parentBottom > window.innerHeight
+      ) {
+        stickyElement.style.position = 'fixed'
+        stickyElement.style.top = `${stickyPoint}px`
+        stickyElement.style.width = '350px'
+      } else if (parentBottom <= window.innerHeight) {
+        const bottomOffset = window.innerHeight - parentBottom
+        const newTop = window.innerHeight - stickyHeight - bottomOffset
+
+        if (newTop < stickyPoint) {
+          stickyElement.style.position = 'absolute'
+          stickyElement.style.top = `${parentRect.height - stickyHeight}px`
+          stickyElement.style.width = '350px'
+        } else {
+          stickyElement.style.position = 'fixed'
+          stickyElement.style.top = `${stickyPoint}px`
+          stickyElement.style.width = '350px'
+        }
+      } else {
+        stickyElement.style.position = 'relative'
+        stickyElement.style.top = '0'
+        stickyElement.style.width = '100%'
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    handleScroll()
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   const teamSocials = useMemo(() => {
     return {
@@ -107,98 +166,9 @@ export default function MissionInfo({
     })
   }, [tab])
 
-  useEffect(() => {
-    if (
-      typeof window === 'undefined' ||
-      window.innerWidth < 800 ||
-      !payRedeemRef.current
-    )
-      return
-
-    let ctx: gsap.Context
-    let originalStyle: Partial<CSSStyleDeclaration> = {}
-
-    async function payRedeemScrollTrigger() {
-      import('gsap/ScrollTrigger').then(({ ScrollTrigger }) => {
-        gsap.registerPlugin(ScrollTrigger)
-
-        ctx = gsap.context(() => {
-          const el = payRedeemRef.current!
-
-          // Save original computed styles
-          const computed = window.getComputedStyle(el)
-          originalStyle = {
-            position: computed.position,
-            top: computed.top,
-            right: computed.right,
-            width: computed.width,
-            zIndex: computed.zIndex,
-          }
-
-          // Main scroll trigger for fixed positioning
-          ScrollTrigger.create({
-            trigger: el,
-            start: 'top bottom',
-            end: 'bottom center',
-            onEnter: () => {
-              gsap.to(el, {
-                position: 'fixed',
-                top: '450px',
-                right: '10vw',
-                width: '300px',
-                zIndex: 50,
-                opacity: 1,
-                duration: 0.3,
-                ease: 'power2.out',
-              })
-            },
-            onLeaveBack: () => {
-              gsap.to(el, {
-                ...originalStyle,
-                opacity: 1,
-                duration: 0.3,
-                ease: 'power2.out',
-              })
-            },
-            onUpdate: (self) => {
-              const windowHeight = window.innerHeight
-              const scrollTop =
-                window.pageYOffset || document.documentElement.scrollTop
-              const documentHeight = document.documentElement.scrollHeight
-
-              // If we're within 500px of the bottom of the page
-              if (documentHeight - (scrollTop + windowHeight) < 500) {
-                gsap.set(el, {
-                  position: 'absolute',
-                  top: documentHeight - 500 - el.offsetHeight,
-                  right: '10vw',
-                  width: '300px',
-                })
-              } else if (self.direction > 0) {
-                // Scrolling down
-                gsap.set(el, {
-                  position: 'fixed',
-                  top: '450px',
-                  right: '10vw',
-                  width: '300px',
-                })
-              }
-            },
-          })
-        })
-      })
-    }
-
-    payRedeemScrollTrigger()
-
-    document.addEventListener('resize', payRedeemScrollTrigger)
-
-    return () => ctx?.revert()
-  }, [])
-
   return (
-    <div ref={containerRef}>
-      <div className="px-[1vw] flex flex-col md:flex-row gap-8 md:gap-2 justify-between max-w-[920px]">
+    <div>
+      <div className="px-[1vw] flex flex-col md:flex-row gap-8 md:gap-2 justify-between max-w-[1000px]">
         <div id="mission-info-tabs" className="mt-4 flex gap-[5vw] w-3/4">
           <MissionInfoTab tab="about" currentTab={tab} setTab={setTab} />
           <MissionInfoTab tab="activity" currentTab={tab} setTab={setTab} />
@@ -242,134 +212,136 @@ export default function MissionInfo({
         </div>
       </div>
 
-      <div id="mission-info-content" className="mt-8 xl:w-3/4 flex gap-4">
-        {tab === 'about' && (
-          <div className="flex gap-4 w-full">
-            <div className="w-full">
-              <MissionInfoHeader
-                title="About the Mission"
-                icon="/assets/icon-star-blue.svg"
-              />
-              <div
-                className="mt-4 prose prose-invert w-full max-w-none"
-                dangerouslySetInnerHTML={{
-                  __html: mission?.metadata?.description || '',
-                }}
-              />
-              <div className="mt-8 flex gap-2 text-light-cool max-w-none">
-                <Image
-                  src="/assets/icon-star-blue.svg"
-                  alt="Star Icon"
-                  width={30}
-                  height={30}
+      <div id="mission-info-content" className="mt-8 w-full flex gap-4">
+        <div className="flex-1 overflow-auto">
+          {tab === 'about' && (
+            <div className="flex gap-4 w-full">
+              <div className="w-full">
+                <MissionInfoHeader
+                  title="About the Mission"
+                  icon="/assets/icon-star-blue.svg"
                 />
-                <h1 className="header font-GoodTimes text-moon-indigo">
-                  About the Team
-                </h1>
-              </div>
-              <div className="mt-4">
-                {teamNFT && (
-                  <StandardWideCard
-                    title={teamNFT?.metadata.name}
-                    link={`/team/${generatePrettyLink(
-                      teamNFT?.metadata?.name || ''
-                    )}`}
-                    subheader={
-                      <div className="flex flex-col gap-2">
-                        <div
-                          id="socials-container"
-                          className="p-1.5 mb-2 mr-2 md:mb-0 px-5 w-fit gap-5 rounded-bl-[10px] rounded-[2vmax] flex text-sm bg-filter"
-                        >
-                          {mission?.metadata?.discord && (
-                            <Link
-                              className="flex gap-2"
-                              href={mission?.metadata?.discord}
-                              target="_blank"
-                              passHref
-                            >
-                              <DiscordIcon />
-                            </Link>
-                          )}
-                          {mission?.metadata?.twitter && (
-                            <Link
-                              className="flex gap-2"
-                              href={mission?.metadata?.twitter}
-                              target="_blank"
-                              passHref
-                            >
-                              <TwitterIcon />
-                            </Link>
-                          )}
-                          {mission?.metadata?.infoUri && (
-                            <Link
-                              className="flex gap-2"
-                              href={mission?.metadata?.infoUri}
-                              target="_blank"
-                              passHref
-                            >
-                              <GlobeAltIcon height={25} width={25} />
-                            </Link>
-                          )}
-                        </div>
-                      </div>
-                    }
-                    paragraph={
-                      <div className="flex flex-col gap-2">
-                        <p>{teamNFT?.metadata.description}</p>
-                      </div>
-                    }
-                    fullParagraph={true}
-                    showMore={false}
-                    showMoreButton={false}
-                    image={teamNFT?.metadata.image}
-                    footer={
-                      <Link
-                        href={`/team/${generatePrettyLink(
-                          teamNFT?.metadata?.name || ''
-                        )}`}
-                        passHref
-                        className="flex gap-2 items-center hover:underline"
-                      >
-                        <InformationCircleIcon width={20} height={20} />
-                        {'Learn more about the team'}
-                      </Link>
-                    }
+                <div
+                  className="mt-4 prose prose-invert w-full max-w-none"
+                  dangerouslySetInnerHTML={{
+                    __html: mission?.metadata?.description || '',
+                  }}
+                />
+                <div className="mt-8 flex gap-2 text-light-cool max-w-none">
+                  <Image
+                    src="/assets/icon-star-blue.svg"
+                    alt="Star Icon"
+                    width={30}
+                    height={30}
                   />
-                )}
+                  <h1 className="header font-GoodTimes text-moon-indigo">
+                    About the Team
+                  </h1>
+                </div>
+                <div className="mt-4">
+                  {teamNFT && (
+                    <StandardWideCard
+                      title={teamNFT?.metadata.name}
+                      link={`/team/${generatePrettyLink(
+                        teamNFT?.metadata?.name || ''
+                      )}`}
+                      subheader={
+                        <div className="flex flex-col gap-2">
+                          <div
+                            id="socials-container"
+                            className="p-1.5 mb-2 mr-2 md:mb-0 px-5 w-fit gap-5 rounded-bl-[10px] rounded-[2vmax] flex text-sm bg-filter"
+                          >
+                            {mission?.metadata?.discord && (
+                              <Link
+                                className="flex gap-2"
+                                href={mission?.metadata?.discord}
+                                target="_blank"
+                                passHref
+                              >
+                                <DiscordIcon />
+                              </Link>
+                            )}
+                            {mission?.metadata?.twitter && (
+                              <Link
+                                className="flex gap-2"
+                                href={mission?.metadata?.twitter}
+                                target="_blank"
+                                passHref
+                              >
+                                <TwitterIcon />
+                              </Link>
+                            )}
+                            {mission?.metadata?.infoUri && (
+                              <Link
+                                className="flex gap-2"
+                                href={mission?.metadata?.infoUri}
+                                target="_blank"
+                                passHref
+                              >
+                                <GlobeAltIcon height={25} width={25} />
+                              </Link>
+                            )}
+                          </div>
+                        </div>
+                      }
+                      paragraph={
+                        <div className="flex flex-col gap-2">
+                          <p>{teamNFT?.metadata.description}</p>
+                        </div>
+                      }
+                      fullParagraph={true}
+                      showMore={false}
+                      showMoreButton={false}
+                      image={teamNFT?.metadata.image}
+                      footer={
+                        <Link
+                          href={`/team/${generatePrettyLink(
+                            teamNFT?.metadata?.name || ''
+                          )}`}
+                          passHref
+                          className="flex gap-2 items-center hover:underline"
+                        >
+                          <InformationCircleIcon width={20} height={20} />
+                          {'Learn more about the team'}
+                        </Link>
+                      }
+                    />
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        )}
-        {tab === 'activity' && (
-          <div className="w-full">
-            <MissionInfoHeader
-              title="Mission Activity"
-              icon="/assets/icon-star-blue.svg"
-            />
-            <MissionTimelineChart
-              points={points}
-              isLoadingPoints={isLoadingPoints}
-              height={500}
-              createdAt={subgraphData?.createdAt}
-            />
-            <MissionActivityList
-              selectedChain={selectedChain}
-              tokenSymbol={token?.tokenSymbol}
-              projectId={mission?.projectId}
-            />
-          </div>
-        )}
-        {tab === 'tokenomics' && (
-          <div className="w-full">
-            <MissionInfoHeader
-              title="Mission Tokenomics"
-              icon="/assets/icon-star-blue.svg"
-            />
-            <MissionTokenInfo mission={mission} token={token} />
-          </div>
-        )}
-        <div className="hidden xl:block">
-          <div className="w-full" ref={payRedeemRef}>
+          )}
+          {tab === 'activity' && (
+            <div className="w-full">
+              <MissionInfoHeader
+                title="Mission Activity"
+                icon="/assets/icon-star-blue.svg"
+              />
+              <MissionTimelineChart
+                points={points}
+                isLoadingPoints={isLoadingPoints}
+                height={500}
+                createdAt={subgraphData?.createdAt}
+              />
+              <MissionActivityList
+                selectedChain={selectedChain}
+                tokenSymbol={token?.tokenSymbol}
+                projectId={mission?.projectId}
+              />
+            </div>
+          )}
+          {tab === 'tokenomics' && (
+            <div className="w-full">
+              <MissionInfoHeader
+                title="Mission Tokenomics"
+                icon="/assets/icon-star-blue.svg"
+              />
+              <MissionTokenInfo mission={mission} token={token} />
+            </div>
+          )}
+        </div>
+        <div className="hidden xl:block  min-w-[350px] lg:w-[400px]">
+          <div ref={stickyRef}>
             <MissionPayRedeem
               stage={stage}
               selectedChain={selectedChain}
