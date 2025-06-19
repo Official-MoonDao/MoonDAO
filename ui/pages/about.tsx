f3ce9a2890e968ebc5284533caf1df3b6a856596
mmoncada1 <moncada@umich.edu>
2025-06-19 15:20:56 -0700

    fixed bug

--- ui/pages/about.tsx
+++ ui/pages/about.tsx
@@ -2,12 +2,13 @@ import Head from '../components/layout/Head'
 
 export default function About() {
   return (
-    <div className="flex flex-col justify-center items-center animate-fadeIn">
+    <div className="flex flex-col justify-center items-center animate-fadeIn h-screen overflow-hidden">
       <Head title="About" />
       <iframe
-        className="absolute top-0 left-0 h-[100vh] overflow-auto w-full"
+        className="absolute top-0 left-0 h-full w-full border-0"
         src="https://docs.moondao.com"
         allowFullScreen
+        style={{ height: '100vh', overflow: 'hidden' }}
       />
     </div>
   )
--- ui/pages/faq.tsx
+++ ui/pages/faq.tsx
@@ -2,12 +2,13 @@ import Head from '../components/layout/Head'
 
 export default function FAQ() {
   return (
-    <div className="flex flex-col justify-center items-center animate-fadeIn">
+    <div className="flex flex-col justify-center items-center animate-fadeIn h-screen overflow-hidden">
       <Head title="FAQ" />
       <iframe
-        className="absolute top-0 left-0 h-[100vh] overflow-auto w-full"
+        className="absolute top-0 left-0 h-full w-full border-0"
         src="https://docs.moondao.com/About/FAQ"
         allowFullScreen
+        style={{ height: '100vh', overflow: 'hidden' }}
       />
     </div>
   )
