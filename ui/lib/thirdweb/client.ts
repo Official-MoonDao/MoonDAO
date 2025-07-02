8dfc62711a21ef84926da454d4ce850f6824a3ca
namedotget <colin.foster4723@gmail.com>
2025-07-02 17:30:13 -0500

    Refactor

--- ui/lib/thirdweb/client.ts
+++ ui/lib/thirdweb/client.ts
@@ -1,29 +1,23 @@
 import { createThirdwebClient } from 'thirdweb'
 
-const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
-  config: {
-    rpc: {
-      batchTimeoutMs: 10000,
-      maxBatchSize: 20,
-      fetch: {
-        requestTimeoutMs: 10000,
-      },
+const rpcConfig = {
+  rpc: {
+    batchTimeoutMs: 10000,
+    maxBatchSize: 20,
+    fetch: {
+      requestTimeoutMs: 10000,
     },
   },
+}
+
+const client = createThirdwebClient({
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
+  config: rpcConfig,
 })
 
 export const serverClient = createThirdwebClient({
   secretKey: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_SECRET as string,
-  config: {
-    rpc: {
-      batchTimeoutMs: 10000,
-      maxBatchSize: 20,
-      fetch: {
-        requestTimeoutMs: 10000,
-      },
-    },
-  },
+  config: rpcConfig,
 })
 
 export default client
