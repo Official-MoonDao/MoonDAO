775ebe36175b6f26aed4f72a977bd5c85af6f3d4
namedotget <colin.foster4723@gmail.com>
2025-07-02 19:06:55 -0500

    Optimize mission profile server-side function

--- ui/lib/thirdweb/client.ts
+++ ui/lib/thirdweb/client.ts
@@ -1,23 +1,25 @@
 import { createThirdwebClient } from 'thirdweb'
 
-const rpcConfig = {
-  rpc: {
-    batchTimeoutMs: 10000,
-    maxBatchSize: 20,
-    fetch: {
-      requestTimeoutMs: 10000,
-    },
-  },
-}
-
 const client = createThirdwebClient({
   clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
-  config: rpcConfig,
+  config: {
+    rpc: {
+      batchTimeoutMs: 5000,
+      maxBatchSize: 50,
+      fetch: {
+        requestTimeoutMs: 5000,
+      },
+    },
+  },
 })
 
 export const serverClient = createThirdwebClient({
   secretKey: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_SECRET as string,
-  config: rpcConfig,
+  config: {
+    rpc: {
+      maxBatchSize: 100,
+    },
+  },
 })
 
 export default client
