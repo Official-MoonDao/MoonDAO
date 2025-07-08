887f1e2dc5153bc7c0b6ff3d2e7b9a9e48ae25fb
namedotget <colin.foster4723@gmail.com>
2025-07-08 15:59:29 -0400

    Adjust formatting

--- ui/components/mission/MissionTokenomicsExplainer.tsx
+++ ui/components/mission/MissionTokenomicsExplainer.tsx
@@ -13,20 +13,20 @@ export default function MissionTokenomicsExplainer() {
 "
       />
 
-      <Point point="Token Allocation" />
-      <div className="ml-8 flex flex-col">
-        <SubPoint point="50% to Contributors" />
-        <SubPoint point="30% to Mission Team" />
-        <SubPoint point="17.5% to MoonDAO" />
-        <SubPoint point="2.5% to LP Pool" />
-      </div>
-
       <Point point="ETH Allocation" />
       <div className="ml-8 flex flex-col">
         <SubPoint point="90% to Mission Team" />
         <SubPoint point="5% to LP Pool" />
         <SubPoint point="2.5% to MoonDAO" />
         <SubPoint point="2.5% to Juicebox" />
+      </div>
+
+      <Point point="Token Allocation" />
+      <div className="ml-8 flex flex-col">
+        <SubPoint point="50% to Contributors" />
+        <SubPoint point="30% to Mission Team" />
+        <SubPoint point="17.5% to MoonDAO" />
+        <SubPoint point="2.5% to LP Pool" />
       </div>
 
       <Point point="Mission Ownership & Security: The mission and all associated funds are fully controlled by your team wallet. If teammates fail to maintain access to the wallets connected to the team multisig, funds may become permanently inaccessible, with no recourse for recovery." />
