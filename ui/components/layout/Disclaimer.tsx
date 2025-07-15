134128b41c195570837f135ffb60929504f9de63
mmoncada1 <158009858+mmoncada1@users.noreply.github.com>
2025-07-15 08:48:15 -0700

    fixed mobile screen bugs (#648)

--- ui/components/layout/Disclaimer.tsx
+++ ui/components/layout/Disclaimer.tsx
@@ -24,54 +24,47 @@ function TokenLink({ href, label }: TokenLinkProps) {
 export default function Disclaimer({ isCentered = false }: DisclaimerProps) {
   return (
     <div className={`w-full ${isCentered ? 'flex justify-center' : ''}`}>
-      <span className="break-words">
-        <div className={`inline-block ${isCentered ? 'text-center' : ''}`}>
-          <span className="opacity-[60%]">
-            <strong>Disclaimer:&nbsp;</strong>
-            There is no expectation of profit with the $MOONEY token. It is a
-            governance token. You are not receiving fractionalized ownership
-            of the DAO's assets in exchange for the token, check the&nbsp;
-          </span>
+      <div className={`text-sm sm:text-base leading-relaxed ${isCentered ? 'text-center' : ''} max-w-none`}>
+        <span className="opacity-[60%]">
+          <strong>Disclaimer:</strong> There is no expectation of profit with the $MOONEY token. It is a governance token. You are not receiving fractionalized ownership of the DAO's assets in exchange for the token, check the{' '}
           <u>
             <Link
               id="FAQ-link"
-              className="opacity-[60%] hover:opacity-[100%]"
+              className="hover:opacity-[100%]"
               href="https://docs.moondao.com/About/FAQ"
               target="_blank"
             >
               FAQ
             </Link>
           </u>
-          <span className="opacity-[60%]">
-            . Always make sure you are interacting with an&nbsp;
-          </span>
+          . Always make sure you are interacting with an{' '}
           <TokenLink
             label="official token address"
             href="https://docs.moondao.com/Governance/Governance-Tokens"
           />
-          <span className="opacity-[60%]">:&nbsp;</span>
+          :{' '}
           <TokenLink
             label="Ethereum"
             href="https://etherscan.io/token/0x20d4DB1946859E2Adb0e5ACC2eac58047aD41395"
           />
-          <span className="opacity-[60%]">,&nbsp;</span>
+          ,{' '}
           <TokenLink
             label="Polygon"
             href="https://polygonscan.com/token/0x74ac7664abb1c8fa152d41bb60e311a663a41c7e"
           />
-          <span className="opacity-[60%]">,&nbsp;</span>
+          ,{' '}
           <TokenLink
             label="Arbitrum"
             href="https://arbiscan.io/token/0x1Fa56414549BdccBB09916f61f0A5827f779a85c"
           />
-          <span className="opacity-[60%]">, or&nbsp;</span>
+          , or{' '}
           <TokenLink
             label="Base"
             href="https://basescan.org/address/0x6585a54A98fADA893904EB8A9E9CDFb927bddf39"
           />
-          <span className="opacity-[60%]">.</span>
-        </div>
-      </span>
+          .
+        </span>
+      </div>
     </div>
   )
 } 
