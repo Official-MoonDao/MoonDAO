51acf282e29524a36f7f1459f7f935b38c75692d
jaderiverstokes <jaderiverstokes@gmail.com>
2025-07-21 12:28:29 -0700

    bug

--- ui/pages/fees.tsx
+++ ui/pages/fees.tsx
@@ -87,7 +87,7 @@ export default function Fees() {
               const balance = await readContract({
                 contract,
                 method: 'balanceOf',
-                params: [address],
+                params: [],
               })
               return balance
             })
