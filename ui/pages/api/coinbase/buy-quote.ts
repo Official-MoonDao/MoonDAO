93ec7f4812c8782e53e8ccb3aa3e2939e0bf52b4
namedotget <colin.foster4723@gmail.com>
2025-08-05 19:07:45 -0500

    Add auth middleware

--- ui/pages/api/coinbase/buy-quote.ts
+++ ui/pages/api/coinbase/buy-quote.ts
@@ -1,4 +1,6 @@
+import { authMiddleware } from 'middleware/authMiddleware'
 import { secureHeaders } from 'middleware/secureHeaders'
+import withMiddleware from 'middleware/withMiddleware'
 import { NextApiRequest, NextApiResponse } from 'next'
 import {
   validateCDPCredentials,
@@ -7,10 +9,7 @@ import {
   BuyQuoteRequest,
 } from '../../../lib/coinbase'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
@@ -83,3 +82,5 @@ export default async function handler(
     return res.status(errorResponse.status).json(errorResponse.body)
   }
 }
+
+export default withMiddleware(handler, authMiddleware)
