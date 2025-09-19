5b8a6a47f418ddcbfad297e71e50ed8383983d60
namedotget <colin.foster4723@gmail.com>
2025-09-19 09:54:21 -0500

    Adjust hsm signer and ownership transfer

--- xp/script/UpgradeXPManager.s.sol
+++ xp/script/UpgradeXPManager.s.sol
@@ -4,22 +4,20 @@ pragma solidity ^0.8.19;
 import "forge-std/Script.sol";
 import "../src/XPManager.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import "./HSMSigner.s.sol";
 
-contract UpgradeXPManagerScript is Script, HSMSigner {
+contract UpgradeXPManagerScript is Script {
     function run() external {
         // Get HSM signer address
-        address hsmSignerAddress = getHSMAddress();
-        console.log("Upgrading with HSM signer:", hsmSignerAddress);
-        
+        address hsmSignerAddress = vm.envAddress("HSM_SIGNER_ADDRESS");
         address proxyAddress = vm.envAddress("XP_MANAGER_ADDRESS");
+        address daoSafeAddress = vm.envAddress("DAO_SAFE_ADDRESS");
 
         if (proxyAddress == address(0)) {
             revert("No proxy address provided");
         }
 
         // Start HSM broadcast
-        startHSMBroadcast();
+        vm.startBroadcast();
 
         // Deploy new XPManager implementation
         XPManager newImplementation = new XPManager();
@@ -29,24 +27,14 @@ contract UpgradeXPManagerScript is Script, HSMSigner {
         // Get the current proxy instance
         XPManager proxy = XPManager(proxyAddress);
         
-        // Verify we're the owner before upgrading
-        address owner = proxy.owner();
-        console.log("Current owner:", owner);
-        console.log("HSM signer is owner:", owner == hsmSignerAddress);
-        
         // Upgrade to new implementation
         proxy.upgradeToAndCall(address(newImplementation), "");
+
+        proxy.transferOwnership(daoSafeAddress);
         
         console.log("Upgrade completed successfully!");
         console.log("New implementation:", address(newImplementation));
 
-        // Log upgrade summary
-        console.log("=== UPGRADE SUMMARY ===");
-        console.log("HSM Signer Address:", hsmSignerAddress);
-        console.log("Proxy Address:", proxyAddress);
-        console.log("New Implementation:", address(newImplementation));
-        console.log("Previous Owner:", owner);
-
-        stopHSMBroadcast();
+        vm.stopBroadcast();
     }
 }
