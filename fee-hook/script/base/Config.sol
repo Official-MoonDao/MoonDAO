506dd2ec03bf618f9323303cd3450bfddbe97ce8
jaderiverstokes <jaderiverstokes@gmail.com>
2025-09-23 13:40:18 -0700

    semicolon

--- fee-hook/script/base/Config.sol
+++ fee-hook/script/base/Config.sol
@@ -42,8 +42,8 @@ contract Config is Script {
     address constant JB_V5_RULESETS = address(0x6292281D69c3593FCF6eA074E5797341476ab428);
     address constant JB_V5_PROJECTS = address(0x885f707EFA18D2cb12f05a3a8eBA6B4B26c8c1D4);
     address constant JB_V5_TERMINAL_STORE = address(0xfE33B439Ec53748C87DcEDACb83f05aDd5014744);
-    address constant JB_V5_TOKENS= address(0x4d0Edd347FB1fA21589C1E109B3474924BE87636)
-    address constant JB_V5_DIRECTORY = address(0x0061E516886A0540F63157f112C0588eE0651dCF)
+    address constant JB_V5_TOKENS= address(0x4d0Edd347FB1fA21589C1E109B3474924BE87636);
+    address constant JB_V5_DIRECTORY = address(0x0061E516886A0540F63157f112C0588eE0651dCF);
     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
     address payable constant CROSS_CHAIN_PAY_ADDRESS = payable(address(0xb9Ce576bec5D36F89275eb9bE6C1057B3bD3572C));
 
