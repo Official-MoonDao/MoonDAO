9331c02747dcaaa9da5578a5e40962ae8fa899c9
jaderiverstokes <jaderiverstokes@gmail.com>
2025-10-23 10:43:13 -0700

    wip

--- subscription-contracts/src/FundingOracle.sol
+++ subscription-contracts/src/FundingOracle.sol
@@ -7,17 +7,6 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FeeHook is FunctionsClient, Ownable {
     mapping(uint256 => uint256) public projectIdToFunding;
-    uint256 public totalWithdrawn;
-    uint256 public totalReceived;
-    bytes32[] public poolIds; // Pools created with this hook
-    bytes32[] public transferredPoolIds; // Pools transferred to other addresses
-    mapping(PoolId => uint256) public uncollectedFees; // Uncollected fees for each LP position
-    mapping(PoolId => uint256) public poolIdToTokenId; // Token ID for the NFT representing each LP position
-    IPositionManager posm;
-    uint256 public minWithdraw = 0.01 ether;
-    uint256 destinationChainId;
-    uint16 destinationEid; // LayerZero endpoint ID
-    address public vMooneyAddress;
 
     // Chainlink
     mapping(bytes32 => address) public requestIdToSender;
@@ -78,40 +67,9 @@ contract FeeHook is FunctionsClient, Ownable {
         "...u256ToBytes(balanceSum),"
         "]);";
 
-    constructor(address owner, IPoolManager _poolManager, IPositionManager _posm, address _lzEndpoint, uint256 _destinationChainId, uint16 _destinationEid, address _vMooneyAddress, address _router, bytes32 _donID, uint64 _subscriptionId) BaseHook(_poolManager) OApp(_lzEndpoint, owner) Ownable(owner) FunctionsClient(_router) {
-        posm = _posm;
-        destinationChainId = _destinationChainId;
-        destinationEid = _destinationEid;
-        vMooneyAddress = _vMooneyAddress;
+    constructor(address owner, address _router, bytes32 _donID, uint64 _subscriptionId) BaseHook(_poolManager) OApp(_lzEndpoint, owner) Ownable(owner) FunctionsClient(_router) {
         donID = _donID;
         subscriptionId = _subscriptionId;
-    }
-
-    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
-        return Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: true,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: false,
-            afterRemoveLiquidity: false,
-            beforeSwap: false,
-            afterSwap: true,
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: true,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
-    }
-
-    function setMinWithdraw(uint256 _minWithdraw) external onlyOwner {
-        minWithdraw = _minWithdraw;
-    }
-
-    function setvMooneyAddress(address _vMooneyAddress) external onlyOwner {
-        vMooneyAddress = _vMooneyAddress;
     }
 
     function setSource(string memory _source) external onlyOwner {
@@ -120,106 +78,6 @@ contract FeeHook is FunctionsClient, Ownable {
 
     function setSubscriptionId(uint64 _subscriptionId) external onlyOwner {
         subscriptionId = _subscriptionId;
-    }
-
-    function sendCrossChain(
-        bytes memory payload,
-        bytes memory options,
-        uint256 messageFee,
-        address refundAddress
-    ) external payable {
-        _lzSend(
-            destinationEid,
-            payload,
-            options,
-            MessagingFee(messageFee, 0),
-            payable(refundAddress)
-        );
-    }
-
-
-    function _afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata _params, BalanceDelta, bytes calldata)
-        internal
-        override
-        returns (bytes4, int128)
-    {
-        PoolId poolId = key.toId();
-        if (_params.zeroForOne && _params.amountSpecified < 0) {
-            uint256 feeAmount = uint256(-_params.amountSpecified) * uint256(key.fee) / 1e6;
-            uncollectedFees[poolId] += feeAmount;
-        }
-        // Only withraw fees if the amount is above a certain threshold to save on gas
-        if (uncollectedFees[poolId] >= minWithdraw) {
-            // 1. Collect fees.
-            bytes memory actions = abi.encodePacked(uint8(Actions.DECREASE_LIQUIDITY), uint8(Actions.TAKE_PAIR));
-            bytes[] memory params = new bytes[](2);
-            uint256 tokenId = poolIdToTokenId[poolId];
-            params[0] = abi.encode(tokenId, 0, 0, 0, bytes(""));
-            params[1] = abi.encode(key.currency0, key.currency1, address(this));
-            posm.modifyLiquiditiesWithoutUnlock(
-                actions, params
-            );
-            // Burn non-native tokens by sending them to the dEaD address
-            if (CurrencyLibrary.balanceOfSelf(key.currency1) > 0) {
-                CurrencyLibrary.transfer(key.currency1, address(0x000000000000000000000000000000000000dEaD), CurrencyLibrary.balanceOfSelf(key.currency1));
-            }
-
-            // 2. Transfer fees to this contract on the destination chain.
-            if (block.chainid != destinationChainId) {
-                bytes memory payload = abi.encode();
-                // Determined empirically
-                uint128 balance = uint128(address(this).balance);
-                uint128 GAS_LIMIT = 500000;
-                // Give an estimate slightly under what we expect
-                uint128 messageFeeEstimate = 500_000_000_000_000;
-                bytes memory optionsEstimate = OptionsBuilder.newOptions().addExecutorLzReceiveOption(GAS_LIMIT, balance - messageFeeEstimate);
-                uint128 messageFee = uint128(ILayerZeroEndpointV2(endpoint).quote(
-                    MessagingParams(destinationEid, peers[destinationEid][0], payload, optionsEstimate, false),
-                    address(this)
-                ).nativeFee);
-                // Correct the message fee with the difference between the estimate and the quote
-                bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(GAS_LIMIT, balance - messageFeeEstimate + balance - messageFee);
-                // Note that external payable functions cannot be called directly
-                // within a contract, so we have to instantiate a new instance
-                // of the FeeHook contract to call the sendCrossChain function.
-                FeeHook(payable(address(this))).sendCrossChain{value: balance}(
-                    payload,
-                    options,
-                    balance,
-                    address(this)
-                );
-            }
-            uncollectedFees[poolId] = 0;
-
-        }
-        return (BaseHook.afterSwap.selector, 0);
-    }
-
-    function _beforeAddLiquidity(
-        address,
-        PoolKey calldata key,
-        IPoolManager.ModifyLiquidityParams calldata,
-        bytes calldata
-    ) internal override returns (bytes4) {
-        PoolId poolId = key.toId();
-        if (poolIdToTokenId[poolId] == 0) {
-            poolIdToTokenId[poolId] = posm.nextTokenId() - 1;
-            poolIds.push(PoolId.unwrap(poolId));
-        }
-        return BaseHook.beforeAddLiquidity.selector;
-    }
-
-    // Allows the owner to transfer an LP position to another address
-    // in case any manual intervention is needed.
-    function transferPosition(
-        address to,
-        bytes32 poolId) external onlyOwner
-    {
-        PoolId id = PoolId.wrap(poolId);
-        uint256 tokenId = poolIdToTokenId[id];
-        require(tokenId != 0, "Token ID not found");
-        transferredPoolIds.push(poolId);
-        PositionManager(payable(address(posm))).safeTransferFrom(address(this), to, tokenId, "");
     }
 
     /**
@@ -265,10 +123,6 @@ contract FeeHook is FunctionsClient, Ownable {
         }
         // Update the contract's state variables with the response and any errors
         (uint256 totalSupply, uint256 userBalance) = abi.decode(response, (uint256, uint256));
-        require(totalSupply > 0, "Total supply is zero");
-        require(userBalance > 0, "User balance is zero");
-        uint256 userProportion = userBalance * 1e18 / totalSupply; // Multiply by 1e18 to preserve precision
-        uint256 allocated = (userProportion * totalReceived) / 1e18; // Divide by 1e18 to normalize
         address withdrawAddress = requestIdToSender[requestId];
         require(withdrawAddress != address(0), "Unknown requestId");
         uint256 withdrawnByUser = totalWithdrawnPerUser[withdrawAddress];
@@ -282,29 +136,5 @@ contract FeeHook is FunctionsClient, Ownable {
         // Emit an event to log the response
         emit Withdraw(requestId, totalSupply, userBalance, withdrawAddress, withdrawAmount);
     }
-
-    function transferETH(address to, uint256 amount) internal {
-        (bool success, ) = to.call{value: amount}("");
-        require(success, "ETH transfer failed");
-    }
-
-    receive() external payable {
-        totalReceived += msg.value;
-    }
-
-    fallback() external payable {
-        totalReceived += msg.value;
-    }
-
-    function _lzReceive(
-        Origin calldata,
-        bytes32,
-        bytes calldata,
-        address,
-        bytes calldata
-    ) internal override {
-        totalReceived += msg.value;
-    }
-
 }
 
