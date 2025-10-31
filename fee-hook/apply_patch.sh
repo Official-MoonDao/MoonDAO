1f6e770325de45b19635ed20f34b80a4e4bb3e76
jaderiverstokes <jaderiverstokes@gmail.com>
2025-10-31 11:45:56 -0700

    typo

--- fee-hook/apply_patch.sh
+++ fee-hook/apply_patch.sh
@@ -2,5 +2,5 @@
 # Juicebox has some contracts which have solc 0.8.23 required, and uniswap v4 requires ^0.8.24.
 # This patch switched juicebox to ^0.8.23 for compatibility.
 echo "Applying patch to submodule..."
-cd fee-hook/node_modules/@bananapus/c-ore
+cd fee-hook/node_modules/@bananapus/core
 git apply ../../0001-patch-version.patch
