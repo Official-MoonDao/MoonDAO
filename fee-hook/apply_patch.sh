820980487a918809932c092f826990ffc7abdf9e
jaderiverstokes <jaderiverstokes@gmail.com>
2025-10-31 14:18:12 -0700

    fix patch

--- fee-hook/apply_patch.sh
+++ fee-hook/apply_patch.sh
@@ -2,5 +2,5 @@
 # Juicebox has some contracts which have solc 0.8.23 required, and uniswap v4 requires ^0.8.24.
 # This patch switched juicebox to ^0.8.23 for compatibility.
 echo "Applying patch to submodule..."
-cd fee-hook/node_modules/@bananapus/core
-git apply ../../../0001-patch-version.patch
+cd fee-hook/node_modules/@bananapus/core/src
+sed -i 's/0.8.23/^0.8.23/g' JBERC20.sol
