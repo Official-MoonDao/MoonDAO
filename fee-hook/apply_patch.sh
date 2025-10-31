16ca00382d14ba2f442a232f850d76e740472048
jaderiverstokes <jaderiverstokes@gmail.com>
2025-10-31 11:40:16 -0700

    fix nana path

--- fee-hook/apply_patch.sh
+++ fee-hook/apply_patch.sh
@@ -2,5 +2,5 @@
 # Juicebox has some contracts which have solc 0.8.23 required, and uniswap v4 requires ^0.8.24.
 # This patch switched juicebox to ^0.8.23 for compatibility.
 echo "Applying patch to submodule..."
-cd fee-hook/lib/nana-core
+cd fee-hook/node_modules/@bananapus/c-ore
 git apply ../../0001-patch-version.patch
