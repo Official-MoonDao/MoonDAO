f0e4ad6f7dcff9e7b1f7aa80d34a67ae84577899
namedotget <colin.foster4723@gmail.com>
2025-11-14 20:47:47 -0600

    Fix rounding issue

--- ui/components/mission/MissionFundingProgressBar.tsx
+++ ui/components/mission/MissionFundingProgressBar.tsx
@@ -9,19 +9,14 @@ export default function MissionFundingProgressBar({
   volume: number
   compact?: boolean
 }) {
-  const stageProgress =
-    volume && fundingGoal ? (volume / (fundingGoal / 1e18)) * 100 : 0
+  const stageProgress = volume && fundingGoal ? (volume / (fundingGoal / 1e18)) * 100 : 0
 
   return (
     <div className={`relative ${compact ? 'mb-2' : 'mb-4'} max-w-[800px]`}>
       <ProgressBar
         height={compact ? '24px' : '32px'}
         progress={stageProgress}
-        label={`${
-          stageProgress < 1
-            ? stageProgress.toFixed(2)
-            : Math.round(stageProgress)
-        }%`}
+        label={`${stageProgress < 1 ? stageProgress.toFixed(2) : Math.floor(stageProgress)}%`}
         compact={compact}
       />
     </div>
