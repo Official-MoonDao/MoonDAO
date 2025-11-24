ccaec30505212e5e4336e1ace1aa81b0c59b2187
Colin Foster <colin.foster4723@gmail.com>
2025-11-24 15:39:17 -0600

    Add team member anon fallback

--- ui/lib/ipfs/gateway.ts
+++ ui/lib/ipfs/gateway.ts
@@ -2,11 +2,24 @@ import { IPFS_GATEWAY } from 'const/config'
 
 export function getIPFSGateway(ipfsString: string) {
   if (!ipfsString) return ''
-  let hash = ipfsString.startsWith('ipfs://')
-    ? ipfsString.split('ipfs://')[1]
-    : ipfsString.startsWith('https://')
-    ? ipfsString.split('/ipfs/')[1]
-    : ipfsString
+
+  // Return local paths and blob URLs as-is
+  if (ipfsString.startsWith('/') || ipfsString.startsWith('blob:')) {
+    return ipfsString
+  }
+
+  // Return full URLs as-is (already have protocol)
+  if (ipfsString.startsWith('http://') || ipfsString.startsWith('https://')) {
+    // Check if it's already an IPFS gateway URL
+    if (ipfsString.includes('/ipfs/')) {
+      return ipfsString
+    }
+    // Otherwise return as-is (could be any external URL)
+    return ipfsString
+  }
+
+  // Process IPFS hashes
+  let hash = ipfsString.startsWith('ipfs://') ? ipfsString.split('ipfs://')[1] : ipfsString
   return `${IPFS_GATEWAY}${hash}`
 }
 
