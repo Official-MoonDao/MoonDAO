cd5ff9b02c74d119cd56f6908318a1a96265a385
mmoncada1 <158009858+mmoncada1@users.noreply.github.com>
2025-12-01 14:13:01 -0500

    Remove proposal status display from Proposal component (#1024)

--- ui/components/nance/Proposal.tsx
+++ ui/components/nance/Proposal.tsx
@@ -36,41 +36,16 @@ export default function Proposal({
       </div>
       <div className="flex justify-between items-center mt-4">
         <div className="flex flex-col items-start">
-          {['Voting', 'Temperature Check'].includes(proposal.status) ? (
-            <div className="flex items-center gap-x-1.5">
-              <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-              </div>
-              <p className="text-sm leading-6 text-white font-RobotoMono font-medium">
-                {proposal.status}
-              </p>
-            </div>
-          ) : votingInfo && votingInfo.state === 'closed' ? (
-            <div className="flex items-center gap-x-1.5">
-              <div className="flex-none rounded-full bg-blue-500/20 p-1">
-                <div className="h-1.5 w-1.5 rounded-full bg-blue-500" />
-              </div>
-              <p className="text-sm leading-6 text-white font-RobotoMono font-medium">
-                Results Available
-              </p>
-            </div>
-          ) : (
-            <>
-              <p className="text-sm leading-6 text-white font-RobotoMono font-medium">
-                {proposal.status}
-              </p>
-              <p className="mt-1 text-xs leading-5 text-gray-400 font-RobotoMono">
-                <span className="sr-only">Last edited</span>
-                <time dateTime={proposal.lastEditedTime}>
-                  {formatDistanceStrict(
-                    new Date(proposal.lastEditedTime || proposal.createdTime),
-                    new Date(),
-                    { addSuffix: true }
-                  )}
-                </time>
-              </p>
-            </>
-          )}
+          <p className="mt-1 text-xs leading-5 text-gray-400 font-RobotoMono">
+            <span className="sr-only">Last edited</span>
+            <time dateTime={proposal.lastEditedTime}>
+              {formatDistanceStrict(
+                new Date(proposal.lastEditedTime || proposal.createdTime),
+                new Date(),
+                { addSuffix: true }
+              )}
+            </time>
+          </p>
         </div>
         <ChevronRightIcon
           className="h-5 w-5 flex-none text-gray-400"
