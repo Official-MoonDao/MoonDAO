701c9a3fde7acd2225372febfc2803dadc23e8ee
name.get <99998750+namedotget@users.noreply.github.com>
2025-12-11 13:21:49 -0600

    Add SpaceBackground and refactor ContentLayout (#1051)
    * Add SpaceBackground and refactor ContentLayout
    
    - Add new SpaceBackground component for animated backgrounds
    - Refactor ContentLayout with improved styling
    - Update Layout and SkeletonLoader components
    - Update all pages to use new layout system
    - Update global styles
    
    * Revert changes passed down from parent branch
    
    * Add useNetworkMistmatch mock
    
    * Add mock for readContract
    
    * Improve space background: make stars more realistic, improve parallax scrolling, improve nebulas and add blue nebula at the top to improve design
    
    * Add css classes for space background
    
    * Remove old background/overlay
    
    * Update styling
    
    * Redesign layout
    
    * Increase timeout to improve test reliability

--- ui/cypress/integration/network/network-page.cy.tsx
+++ ui/cypress/integration/network/network-page.cy.tsx
@@ -183,7 +183,7 @@ describe('<Network />', () => {
         </TestnetProviders>
       )
 
-      cy.contains('Map').click()
+      cy.contains('Map', { timeout: 10000 }).should('be.visible').click()
       cy.get('#network-content').should('exist')
     })
   })
