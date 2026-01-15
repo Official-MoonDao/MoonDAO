9fbaacb8c3a3791fe060e180b935f6b3dee0e250
Miguel Moncada-Larrotiz <mmoncada105@gmail.com>
2026-01-15 13:47:14 -0500

    PFP -> Profile

--- ui/components/subscription/CitizenProfileLink.tsx
+++ ui/components/subscription/CitizenProfileLink.tsx
@@ -1,39 +1,31 @@
-import { useRouter } from 'next/router'
-import { useContext, useState } from 'react'
+import { useContext } from 'react'
 import CitizenContext from '@/lib/citizen/citizen-context'
+import { generatePrettyLinkWithId } from '@/lib/subscription/pretty-links'
 import IPFSRenderer from '../layout/IPFSRenderer'
-import { LoadingSpinner } from '../layout/LoadingSpinner'
 
 export default function CitizenProfileLink() {
-  const router = useRouter()
   const { citizen } = useContext(CitizenContext)
 
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-
   if (citizen?.metadata?.name) {
+    const name = citizen.metadata.name
+    const id = citizen.metadata.id
+    const profileUrl = `/citizen/${generatePrettyLinkWithId(name, id)}`
+    
     return (
-      <button
-        onClick={async () => {
-          setIsLoading(true)
-          await router.push('/')
-          setIsLoading(false)
-        }}
+      <a
+        href={profileUrl}
         className="flex items-center justify-center"
       >
         <div className="rounded-[100%] w-[40px] h-[40px] overflow-hidden animate-fadeIn flex items-center justify-center">
-          {isLoading ? (
-            <LoadingSpinner />
-          ) : (
-            <IPFSRenderer
-              className="object-cover"
-              src={citizen.metadata.image}
-              width={40}
-              height={40}
-              alt="Citizen Image"
-            />
-          )}
+          <IPFSRenderer
+            className="object-cover"
+            src={citizen.metadata.image}
+            width={40}
+            height={40}
+            alt="Citizen Image"
+          />
         </div>
-      </button>
+      </a>
     )
   }
 }
