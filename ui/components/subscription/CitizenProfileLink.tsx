706810814864f84154cee503990db0212a024bf3
Miguel Moncada-Larrotiz <mmoncada105@gmail.com>
2026-01-15 15:11:52 -0500

    restore spinner

--- ui/components/subscription/CitizenProfileLink.tsx
+++ ui/components/subscription/CitizenProfileLink.tsx
@@ -1,10 +1,12 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import CitizenContext from '@/lib/citizen/citizen-context'
 import { generatePrettyLinkWithId } from '@/lib/subscription/pretty-links'
 import IPFSRenderer from '../layout/IPFSRenderer'
+import { LoadingSpinner } from '../layout/LoadingSpinner'
 
 export default function CitizenProfileLink() {
   const { citizen } = useContext(CitizenContext)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   if (citizen?.metadata?.name) {
     const name = citizen.metadata.name
@@ -14,16 +16,21 @@ export default function CitizenProfileLink() {
     return (
       <a
         href={profileUrl}
+        onClick={() => setIsLoading(true)}
         className="flex items-center justify-center"
       >
         <div className="rounded-[100%] w-[40px] h-[40px] overflow-hidden animate-fadeIn flex items-center justify-center">
-          <IPFSRenderer
-            className="object-cover"
-            src={citizen.metadata.image}
-            width={40}
-            height={40}
-            alt="Citizen Image"
-          />
+          {isLoading ? (
+            <LoadingSpinner />
+          ) : (
+            <IPFSRenderer
+              className="object-cover"
+              src={citizen.metadata.image}
+              width={40}
+              height={40}
+              alt="Citizen Image"
+            />
+          )}
         </div>
       </a>
     )
