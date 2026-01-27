7dd92ae14d3c3711b2dca9bfa42cb97551ce1463
pmoncada <pmoncada@umich.edu>
2026-01-27 11:08:42 -0800

    fix earth and moon tests

--- ui/cypress/integration/globe/earth.cy.tsx
+++ ui/cypress/integration/globe/earth.cy.tsx
@@ -3,6 +3,7 @@ import Earth from '@/components/globe/Earth'
 describe('<Earth />', () => {
   it('Renders component', () => {
     cy.mount(<Earth pointsData={[]} />)
-    cy.get('canvas').should('exist')
+    // WebGL/Three.js canvas creation is async - needs time to initialize context and load textures
+    cy.get('canvas', { timeout: 10000 }).should('exist').and('be.visible')
   })
 })
--- ui/cypress/integration/globe/moon.cy.tsx
+++ ui/cypress/integration/globe/moon.cy.tsx
@@ -3,6 +3,7 @@ import Moon from '@/components/globe/Moon'
 describe('<Moon />', () => {
   it('Renders component', () => {
     cy.mount(<Moon />)
-    cy.get('canvas').should('exist')
+    // WebGL/Three.js canvas creation is async - needs time to initialize context and load textures
+    cy.get('canvas', { timeout: 10000 }).should('exist').and('be.visible')
   })
 })
