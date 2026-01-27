d0b9d9159d20079eb9716241ec116e3c733d23f2
pmoncada <pmoncada@umich.edu>
2026-01-27 11:38:05 -0800

    actually fix them... hopefully?

--- ui/cypress/integration/globe/earth.cy.tsx
+++ ui/cypress/integration/globe/earth.cy.tsx
@@ -1,37 +1,10 @@
-import Globe from 'react-globe.gl'
-import useGlobeSize from '@/lib/globe/useGlobeSize'
-
-// Test wrapper that exposes onGlobeReady for test synchronization
-function TestEarth({ onReady }: { onReady?: () => void }) {
-  const size = useGlobeSize()
-  return (
-    <Globe
-      width={size.width}
-      height={size.height}
-      backgroundColor="#00000000"
-      globeImageUrl={'/react-globe/earth-night.jpg'}
-      pointsData={[]}
-      onGlobeReady={onReady}
-    />
-  )
-}
+import Earth from '@/components/globe/Earth'
 
 describe('<Earth />', () => {
   it('Renders component with canvas', () => {
-    // Use a ref to track when globe is ready
-    let globeReady = false
-    const onReady = () => {
-      globeReady = true
-    }
+    cy.mount(<Earth pointsData={[]} />)
 
-    cy.mount(<TestEarth onReady={onReady} />)
-
-    // Wait for globe to be ready (WebGL initialized, textures loaded)
-    cy.wrap(null, { timeout: 15000 }).should(() => {
-      expect(globeReady).to.be.true
-    })
-
-    // Canvas should now exist
-    cy.get('canvas').should('exist')
+    // Wait for canvas to render (Globe component creates a canvas element)
+    cy.get('canvas', { timeout: 15000 }).should('exist')
   })
 })
--- ui/cypress/integration/globe/moon.cy.tsx
+++ ui/cypress/integration/globe/moon.cy.tsx
@@ -1,45 +1,10 @@
-import Globe from 'react-globe.gl'
-import useGlobeSize from '@/lib/globe/useGlobeSize'
-import MOON_LANDINGS from '../../../public/react-globe/moon_landings.json'
-
-// Test wrapper that exposes onGlobeReady for test synchronization
-function TestMoon({ onReady }: { onReady?: () => void }) {
-  const size = useGlobeSize()
-  return (
-    <Globe
-      width={size.width}
-      height={size.height}
-      backgroundColor="#00000000"
-      globeImageUrl={'/react-globe/lunar_surface.jpg'}
-      bumpImageUrl={'/react-globe/lunar_bumpmap.jpg'}
-      showGraticules={false}
-      showAtmosphere={false}
-      labelText="label"
-      labelSize={1.25}
-      labelDotRadius={0.4}
-      labelsData={MOON_LANDINGS}
-      labelColor="#000000"
-      onGlobeReady={onReady}
-    />
-  )
-}
+import Moon from '@/components/globe/Moon'
 
 describe('<Moon />', () => {
   it('Renders component with canvas', () => {
-    // Use a ref to track when globe is ready
-    let globeReady = false
-    const onReady = () => {
-      globeReady = true
-    }
+    cy.mount(<Moon />)
 
-    cy.mount(<TestMoon onReady={onReady} />)
-
-    // Wait for globe to be ready (WebGL initialized, textures loaded)
-    cy.wrap(null, { timeout: 15000 }).should(() => {
-      expect(globeReady).to.be.true
-    })
-
-    // Canvas should now exist
-    cy.get('canvas').should('exist')
+    // Wait for canvas to render (Globe component creates a canvas element)
+    cy.get('canvas', { timeout: 15000 }).should('exist')
   })
 })
