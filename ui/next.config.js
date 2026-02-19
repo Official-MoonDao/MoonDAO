03be4e05558b0b101d9eae3c74b60af98b70d1a4
ryand2d <rshauers@gmail.com>
2026-02-19 16:12:44 -0500

    overview to luma redirect

--- ui/next.config.js
+++ ui/next.config.js
@@ -200,6 +200,11 @@ module.exports = withBundleAnalyzer(
             permanent: true,
           },
           {
+            source: '/zero-g',
+            destination: '/zero-gravity',
+            permanent: true,
+          },
+          {
             source: '/zero-g-sweepstakes',
             destination: '/zero-gravity',
             permanent: true,
@@ -365,11 +370,6 @@ module.exports = withBundleAnalyzer(
             source: '/overview',
             destination: 'https://luma.com/1vvxgrcz',
             permanent: false,
-          },
-          {
-            source: '/zero-g',
-            destination: '/zero-gravity',
-            permanent: true,
           },
           // Citizen specific referral redirects
           {
