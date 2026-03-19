62fc9403d57446d57b61346c32d443d242721cdc
pmoncada <pmoncada@umich.edu>
2026-03-19 10:44:01 -0700

    remove non-public news, add descriptions, add images

--- ui/next.config.js
+++ ui/next.config.js
@@ -8,7 +8,7 @@ const cspHeaderBase = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' https://fonts.googleapis.com https://www.googletagmanager.com https://pay.google.com https://*.lu.ma https://lu.ma https://*.luma.com https://luma.com;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.lu.ma https://lu.ma https://*.luma.com https://luma.com;
-    img-src 'self' blob: data: https://*.ipfscdn.io https://r2.comfy.icu https://ipfs.io https://cdn.discordapp.com https://cdn.stamp.fyi https://cdn.shopify.com https://cryptologos.cc https://ipfs.cf-ipfs.com https://*.walletconnect.com https://unpkg.com ipfs://* https://gray-main-toad-36.mypinata.cloud  https://safe-transaction-assets.safe.global https://*.lu.ma https://lu.ma https://*.luma.com https://luma.com;
+    img-src 'self' blob: data: https://*.ipfscdn.io https://r2.comfy.icu https://ipfs.io https://cdn.discordapp.com https://cdn.stamp.fyi https://cdn.shopify.com https://cryptologos.cc https://ipfs.cf-ipfs.com https://*.walletconnect.com https://unpkg.com ipfs://* https://gray-main-toad-36.mypinata.cloud  https://safe-transaction-assets.safe.global https://*.lu.ma https://lu.ma https://*.luma.com https://luma.com https://*.lumacdn.com https://images.lumacdn.com https://pbs.twimg.com https://img.youtube.com;
     font-src 'self' https://fonts.gstatic.com https://*.lu.ma https://lu.ma https://*.luma.com https://luma.com;
     object-src 'none';
     base-uri 'self';
@@ -82,6 +82,9 @@ module.exports = withBundleAnalyzer(
           'test.com',
           'embed.filekitcdn.com',
           'functions-js.kit.com',
+          'images.lumacdn.com',
+          'pbs.twimg.com',
+          'img.youtube.com',
         ],
         formats: ['image/avif', 'image/webp'],
       },
